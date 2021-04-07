2138a739ee580da2966dcb5cf4a225142738617f
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-04-07 10:18:30 -0700

    Update and rename All Over the World - The Very Best of ELO.bat to All Over the World - The Best of ELO.bat
    Change copyright year to release year of the album

--- Amazon Music Conversion (FFmpeg)/Electric Light Orchestra (ELO)/All Over the World - The Best of ELO.bat
+++ Amazon Music Conversion (FFmpeg)/Electric Light Orchestra (ELO)/All Over the World - The Best of ELO.bat
@@ -5,7 +5,7 @@ mkdir "All Over the World - The Best of ELO [Converted]"
 ffmpeg -y -i "01 - Mr. Blue Sky.mp3" -vf scale=512:512 -sws_flags bicubic cover_tmp.png
 
 :: Import song, add resized cover art, remove all current metadata, attach the song + cover art to the output file, register new metadata, initialize second part of adding cover art, encode audio into MP3, set sample rate to 44100 kHZ, set audio bitrate to 160kb, and output the new file.
-ffmpeg -y -i "01 - Mr. Blue Sky.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="Electric Light Orchestra (ELO)" -metadata album="All Over the World: The Very Best of ELO" -metadata title="Mr. Blue Sky" -metadata genre="Rock/Pop" -metadata copyright="© 1978, Electric Light Orchestra" -metadata track="1/20" -c:1 png -disposition:1 attached_pic -c:a aac -ar 44100 -b:a 160k "Mr. Blue Sky.m4a"
+ffmpeg -y -i "01 - Mr. Blue Sky.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="Electric Light Orchestra (ELO)" -metadata album="All Over the World: The Best of ELO" -metadata title="Mr. Blue Sky" -metadata genre="Classical/Pop" -metadata copyright="© 2005, Electric Light Orchestra" -metadata track="1/20" -c:1 png -disposition:1 attached_pic -c:a aac -ar 44100 -b:a 160k "Mr. Blue Sky.m4a"
 
 :: Delete the temporary cover art file.
 DEL cover_tmp.png
