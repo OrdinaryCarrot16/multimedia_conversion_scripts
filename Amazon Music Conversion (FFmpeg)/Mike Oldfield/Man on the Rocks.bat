aed0e32b6f19cfaee003c37cea53c1cbcf81c6cd
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-04-07 13:13:23 -0700

    Update Man on the Rocks.bat
    Add language metadata

--- Amazon Music Conversion (FFmpeg)/Mike Oldfield/Man on the Rocks.bat
+++ Amazon Music Conversion (FFmpeg)/Mike Oldfield/Man on the Rocks.bat
@@ -5,7 +5,7 @@ mkdir "Man on the Rocks [Converted]"
 ffmpeg -y -i "07 - Nuclear.mp3" -vf scale=512:512 -sws_flags bicubic cover_tmp.png
 
 :: Import song, add resized cover art, remove all current metadata, attach the song + cover art to the output file, register new metadata, initialize second part of adding cover art, encode audio into AAC, set sample rate to 44100 kHZ, set audio bitrate to 160kb, and output the new file.
-ffmpeg -y -i "07 - Nuclear.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="Mike Oldfield" -metadata album="Man on the Rocks" -metadata title="Nuclear" -metadata genre="Pop" -metadata copyright="© 2014, Mike Oldfield" -metadata track="7/11" -c:1 png -disposition:1 attached_pic -c:a aac -ar 44100 -b:a 160k "Nuclear.m4a"
+ffmpeg -y -i "07 - Nuclear.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="Mike Oldfield" -metadata album="Man on the Rocks" -metadata title="Nuclear" -metadata genre="Pop" -metadata language="English" -metadata copyright="© 2014, Mike Oldfield" -metadata track="7/11" -c:1 png -disposition:1 attached_pic -c:a aac -ar 44100 -b:a 160k "Nuclear.m4a"
 
 :: Move new files to said directory.
 move "Nuclear.m4a" "%CD%\Man on the Rocks [Converted]"
