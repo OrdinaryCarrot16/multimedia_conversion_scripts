dd0dcd600cbb293f77f713af276d0e5c98b44fcf
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-04-07 14:10:46 -0700

    Update Man on the Rocks.bat
    Fixed Typo

--- Amazon Music Conversion (FFmpeg)/Mike Oldfield/Man on the Rocks.bat
+++ Amazon Music Conversion (FFmpeg)/Mike Oldfield/Man on the Rocks.bat
@@ -4,7 +4,7 @@ mkdir "Man on the Rocks [Converted]"
 :: Copy cover art from first song, then resize it to 512x512 using a bicubic algorithim.
 ffmpeg -y -i "07 - Nuclear.mp3" -vf scale=512:512 -sws_flags bicubic cover_tmp.png
 
-:: Import song, add resized cover art, remove all current metadata, attach the song + cover art to the output file, register new metadata, initialize second part of adding cover art, encode audio into AAC, set sample rate to 44100 kHZ, set audio bitrate to 160kb, and output the new file.
+:: Import song, add resized cover art, remove all current metadata, attach the song + cover art to the output file, register new metadata, initialize second part of adding cover art, encode audio into MP3, set sample rate to 44100 kHZ, set audio bitrate to 160kb, and output the new file.
 ffmpeg -y -i "07 - Nuclear.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="Mike Oldfield" -metadata album="Man on the Rocks" -metadata title="Nuclear" -metadata genre="Pop" -metadata language="English" -metadata copyright="© 2014, Mike Oldfield" -metadata track="7/11" -c:1 png -disposition:1 attached_pic -c:a libmp3lame -ar 44100 -b:a 160k "Nuclear.mp3"
 
 :: Move new files to said directory.
