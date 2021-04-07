1bb75d916700a9da5ab25dac61dd51791a409303
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-04-07 13:59:37 -0700

    Update No Regrets.bat
    Fixed typo

--- Amazon Music Conversion (FFmpeg)/Midge Ure/No Regrets.bat
+++ Amazon Music Conversion (FFmpeg)/Midge Ure/No Regrets.bat
@@ -5,7 +5,7 @@ mkdir "No Regrets [Converted]"
 ffmpeg -y -i "05 - The Man Who Sold the World.mp3" -vf scale=512:512 -sws_flags bicubic cover_tmp.png
 
 :: Import song, add resized cover art, remove all current metadata, attach the song + cover art to the output file, register new metadata, initialize second part of adding cover art, encode audio into AAC, set sample rate to 44100 kHZ, set audio bitrate to 160kb, and output the new file.
-ffmpeg -y -i "05 - The Man Who Sold the World.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="Midge Ure" -metadata album="No Regrets" -metadata title="The Man Who Sold the World" -metadata genre="Pop" -metadata language"English" -metadata copyright="© 2003, Midge Ure" -metadata track="5/17" -c:1 png -disposition:1 attached_pic -c:a libmp3lame -ar 44100 -b:a 160k "The Man Who Sold the World.mp3"
+ffmpeg -y -i "05 - The Man Who Sold the World.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="Midge Ure" -metadata album="No Regrets" -metadata title="The Man Who Sold the World" -metadata genre="Pop" -metadata language="English" -metadata copyright="© 2003, Midge Ure" -metadata track="5/17" -c:1 png -disposition:1 attached_pic -c:a libmp3lame -ar 44100 -b:a 160k "The Man Who Sold the World.mp3"
 
 :: Move new files to said directory.
 move "The Man Who Sold the World.mp3" "%CD%\No Regrets [Converted]"
