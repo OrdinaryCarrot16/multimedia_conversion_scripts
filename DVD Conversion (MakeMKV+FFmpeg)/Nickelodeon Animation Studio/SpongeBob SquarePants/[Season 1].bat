4caedcf37349a13b4d5212fad785d98e836923bd
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-04-11 10:10:17 -0700

    Delete DVD Conversion (MakeMKV+FFmpeg)/Nickelodeon Animation Studio directory

--- DVD Conversion (MakeMKV+FFmpeg)/Nickelodeon Animation Studio/SpongeBob SquarePants/[Season 1].bat
+++
@@ -1,3 +0,0 @@
-::ffmpeg -i "title_t00.mkv" -ss 00:01:38 -vf scale=720:480 -sws_flags bicubic -vframes 1 thumbnail_tmp.png
-ffmpeg -i "title_t00.mkv" -map_metadata -1 -map 0:0 -map 0:1 -metadata show="SpongeBob SquarePants" -metadata title="Reef Blower" -c:v libx264 -preset medium -tune animation -crf 18 -c:a aac -b:a 160k "Reef Blower.mp4"
-pause
