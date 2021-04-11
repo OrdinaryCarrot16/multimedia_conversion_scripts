4e63f99a75687653086ed27a328972872a92da91
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-04-10 23:25:12 -0700

    Update The Best of B.J. Thomas.bat

--- Amazon Music Conversion (FFmpeg)/B.J. Thomas/The Best of B.J. Thomas.bat
+++ Amazon Music Conversion (FFmpeg)/B.J. Thomas/The Best of B.J. Thomas.bat
@@ -7,7 +7,7 @@ mkdir "The Best of BJ Thomas [Converted]"
 ffmpeg -y -i "01 - Raindrops Keep Falling On My Head.mp3" -vf scale=512:512 -sws_flags bicubic cover_tmp.png
 
 :: Import song, add resized cover art, remove all current metadata, attach the song + cover art to the output file, register new metadata, initialize second part of adding cover art, encode audio into MP3, set sample rate to 44100 kHZ, set audio bitrate to 160kb, and output the new file.
-ffmpeg -y -i "01 - Raindrops Keep Falling On My Head.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="B.J. Thomas" -metadata album="The Best of B.J. Thomas" -metadata title="Raindrops Keep Falling on My Head" -metadata genre="Classical" -metadata language="English" -metadata copyright="© 2011, B.J. Thomas" -metadata track="1/14" -c:1 png -disposition:1 attached_pic -c:a libmp3lame -ar 44100 -b:a 160k "Raindrops Keep Falling on My Head.mp3"
+ffmpeg -y -i "01 - Raindrops Keep Falling On My Head.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="B.J. Thomas" -metadata album="The Best of B.J. Thomas" -metadata title="Raindrops Keep Falling on My Head" -metadata genre="Classical" -metadata language="English" -metadata copyright="© 2011, B.J. Thomas" -metadata track="1/14" -metadata encoder="FFmpeg" -c:1 png -disposition:1 attached_pic -c:a libmp3lame -ar 44100 -b:a 160k "Raindrops Keep Falling on My Head.mp3"
 
 :: Move new files to said directory.
 move "Raindrops Keep Falling on My Head.mp3" "%CD%\The Best of BJ Thomas [Converted]"
