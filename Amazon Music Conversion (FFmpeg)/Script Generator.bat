3e9b761b0a41f4fe9960dddc5217d887471313b9
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-04-12 13:34:17 -0700

    Update Script Generator.bat

--- Amazon Music Conversion (FFmpeg)/Script Generator.bat
+++ Amazon Music Conversion (FFmpeg)/Script Generator.bat
@@ -9,6 +9,7 @@ if %track_amount% GTR 1 (
     set /P album=Album Name: 
 )
 
+set /P filename=Song File Name: 
 set /P title=Song Name: 
 set /P track=Track Number: 
 set /P genre=Genre: 
@@ -16,6 +17,6 @@ set /P language=Language:
 
 ffmpeg -i "%cover-art%" -vf scale=512:512 -sws_flags bicubic cover_tmp.png
 
-ffmpeg -i "%song%" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="%artist%" -metadata album="%album%" -metadata title="%title%" -metadata track="%track%/%track_amount%" -metadata genre="%genre%" -metadata language="%language%" -c:1 png -disposition:1 attached_pic -c:a libmp3lame -ar 44100 -b:a 160k "%title%.mp3"
+ffmpeg -i "%filename%" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="%artist%" -metadata album="%album%" -metadata title="%title%" -metadata track="%track%/%track_amount%" -metadata genre="%genre%" -metadata language="%language%" -c:1 png -disposition:1 attached_pic -c:a libmp3lame -ar 44100 -b:a 160k "%title%.mp3"
 
 pause
