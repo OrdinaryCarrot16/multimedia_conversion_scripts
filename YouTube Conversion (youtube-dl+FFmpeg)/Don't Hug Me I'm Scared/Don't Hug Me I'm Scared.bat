9de4b2738e5111f9f68be95bea75b12f28e85214
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-04-18 14:07:37 -0700

    Create Don't Hug Me I'm Scared.bat

---
+++ YouTube Conversion (youtube-dl+FFmpeg)/Don't Hug Me I'm Scared/Don't Hug Me I'm Scared.bat
@@ -0,0 +1,6 @@
+youtube-dl --embed-subs -f 244+251 https://www.youtube.com/watch?v=9C_HReR_McQ
+youtube-dl --embed-subs -f 244+251 https://www.youtube.com/watch?v=vtkGtXtDlQA
+youtube-dl --embed-subs -f 244+251 https://www.youtube.com/watch?v=sXOdn6vLCuU
+youtube-dl --embed-subs -f 244+251 https://www.youtube.com/watch?v=G9FGgwCQ22w
+youtube-dl --embed-subs -f 244+251 https://www.youtube.com/watch?v=tS_Xq7gSCBM
+youtube-dl --embed-subs -f 244+251 https://www.youtube.com/watch?v=dbL-NSkXnl8
