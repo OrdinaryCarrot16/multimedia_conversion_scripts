47694ef029d61a22d11d395bff6ed10ac59ad489
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-04-19 10:32:46 -0700

    Update Script Generator (Semi-Manual).bat
    Retrieve file name from YouTube video

--- YouTube Conversion (youtube-dl+FFmpeg)/Script Generator (Semi-Manual).bat
+++ YouTube Conversion (youtube-dl+FFmpeg)/Script Generator (Semi-Manual).bat
@@ -1,7 +1,7 @@
 setlocal enableDelayedExpansion
 @echo off
 
-set /A TRACK_COUNTER=0+1
+set /A TRACK_COUNTER=0
 set /P channel=YouTube Channel Name: 
 set /P language=Language: 
 set /P media_type=Is it a movie/single video or a TV show/series (type 'movie' or 'tv' depending on what the content is): 
@@ -30,7 +30,7 @@ if %media_type%==tv (
 	echo.>>"!tv_show! [Generated].bat"
 )
 
-for /L %%e in (1,1,!episode_amount!) do set /P url=Enter YouTube URL: & set /P resolution=What video resolution (242 (240p), 243 (360p), 244 (480p), 245 (720p), etc.): & echo youtube-dl --embed-subs -f !resolution!+251 !url! >>"!tv_show! [Generated].bat" & set /P filename=Video File Name (!TRACK_COUNTER!/!episode_amount!): & set /P title=Video Name: & set /P genre=Genre: & echo ffmpeg -i "!filename!" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata show="!tv_show!" -metadata media_type="tv" -metadata season_number="!season_id!" -metadata episode_id="!episode_id!" -metadata artist="!channel!" -metadata title="!title!" -metadata track="!TRACK_COUNTER!/%episode_amount%" -metadata genre="!genre!" -metadata language="%language%" -threads 2 -c:a aac -profile:a aac_main -ac 2 -c:v libx264 -ar 44100 -b:a 160k -preset medium -profile:v main -level 3.0 -crf 20 -maxrate 8000k -bufsize 8000k -movflags +faststart "!title!.mp4" >>"!tv_show! [Generated].bat" & echo move "!title!.mp3" "%CD%\!album! [Converted]" >>"!album! [Generated].bat" (
+for /L %%e in (1,1,!episode_amount!) do set /A TRACK_COUNTER+=1 & set /P url=Enter YouTube URL (!TRACK_COUNTER!/!episode_amount!): & set /P resolution=What video resolution (242 (240p), 243 (360p), 244 (480p), 245 (720p), etc.): & echo youtube-dl --embed-subs -f !resolution!+251 !url! >>"!tv_show! [Generated].bat" & youtube-dl --get-filename !url! & set /P filename=Video File Name: & set /P title=Video Name: & set /P genre=Genre: & echo ffmpeg -i "!filename!" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata show="!tv_show!" -metadata media_type="tv" -metadata season_number="!season_id!" -metadata episode_id="!episode_id!" -metadata artist="!channel!" -metadata title="!title!" -metadata track="!TRACK_COUNTER!/%episode_amount%" -metadata genre="!genre!" -metadata language="%language%" -threads 2 -c:a aac -profile:a aac_main -ac 2 -c:v libx264 -ar 44100 -b:a 160k -preset medium -profile:v main -level 3.0 -crf 20 -maxrate 8000k -bufsize 8000k -movflags +faststart "!title!.mp4" >>"!tv_show! [Generated].bat" & echo move "!title!.mp4" "%CD%\!tv_show! [Converted]" >>"!tv_show! [Generated].bat" (
 	echo.>>"!tv_show! [Generated].bat" (
 	echo pause >>"!tv_show! [Generated].bat"
 )
