36c1c321fffd11f92e6f05bf6a26444156070e1b
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-05-07 12:57:04 -0700

    Update Season 1 - Disc 1.bat

--- DVD Conversion (MakeMKV+FFmpeg)/Cartoon Network/Adventure Time/Season 1 - Disc 1.bat
+++ DVD Conversion (MakeMKV+FFmpeg)/Cartoon Network/Adventure Time/Season 1 - Disc 1.bat
@@ -1,1 +1,3 @@
-
+:: Extract intro
+ffmpeg -y -avoid_negative_ts make_non_negative -ss 00:00:01 -to 00:00:24.900 -i "ADVENTURE TIME SEASON 1 DISC 1_t00.mkv" -c:v libx264 -crf 16 -preset veryfast -c:a aac -b:a 192k intro_tmp.mkv
+pause
