a1ba7505ca56692048480ecf2b3050359b496972
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-05-07 13:18:51 -0700

    Update Season 1 - Disc 1.bat
    Fix typo

--- DVD Conversion (MakeMKV+FFmpeg)/Cartoon Network/Adventure Time/Season 1 - Disc 1.bat
+++ DVD Conversion (MakeMKV+FFmpeg)/Cartoon Network/Adventure Time/Season 1 - Disc 1.bat
@@ -3,7 +3,7 @@ ffmpeg -y -f lavfi -i color=size=720x480:color=black:rate=30 -f lavfi -i anullsr
 ffmpeg -y -f lavfi -i color=size=720x480:color=black:rate=30 -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -c:a aac -c:v libx264 -t 2 spacer_2_tmp.mkv
 
 :: Extract intro
-ffmpeg -y -avoid_negative_ts make_non_negative -ss 00:00:01 -to 00:00:24.900 -i "ADVENTURE TIME SEASON 1 DISC 1_t00.mkv" -c:v libx264 -crf 16 -preset veryfast -c:a aac -b:a 192k intro_tmp.mkv
+ffmpeg -y -avoid_negative_ts make_non_negative -ss 00:00:01 -to 00:00:24.900 -i "ADVENTURE TIME SEASON 1 DISC 1_t00.mkv" -c:v libx264 -crf 8 -preset veryfast -c:a aac -b:a 192k intro_tmp.mkv
 
 :: Extract title card, episode, and credits
 ffmpeg -y -avoid_negative_ts make_non_negative -ss 00:00:25.850 -to 00:00:32.500 -i "ADVENTURE TIME SEASON 1 DISC 1_t00.mkv" -c:v libx264 -crf 8 -preset veryfast -c:a aac -b:a 192k title_card_tmp.mkv
