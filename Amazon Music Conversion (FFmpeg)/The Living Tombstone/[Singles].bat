41ca949a3382d5665f26759af0076ba68b08e7ec
Ordin <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-10-10 00:37:42 -0700

    Update [Singles].bat

--- Amazon Music Conversion (FFmpeg)/The Living Tombstone/[Singles].bat
+++ Amazon Music Conversion (FFmpeg)/The Living Tombstone/[Singles].bat
@@ -8,7 +8,7 @@ ffmpeg -y -i "01 - Love I Need.mp3" -vf scale=480:480 -sws_flags bicubic cover_T
 
 :: Import song, add resized cover art, remove all current metadata, attach the song + cover art to the output file, register new metadata, initialize second part of adding cover art, encode audio into MP3, set sample rate to 44100 kHZ, set audio bitrate to 160kb, and output the new file.
 ffmpeg -i "01 - Love I Need.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata title="Love I Need"  -metadata track="1/1" -metadata genre="Electronic/Pop" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -movflags +faststart "Love I Need.m4a"
-move "Love I Need.mp3" "[Singles] [Converted]"
+move "Love I Need.m4a" "[Singles] [Converted]"
 
 :: Stop the script, giving the user a chance to make sure everything looks good before proceeding to quit.
 pause
