d32fe0badedc5eb6747400b272937d0f2cc54a0b
Ordin <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-10-10 00:34:56 -0700

    Update [Singles].bat

--- Amazon Music Conversion (FFmpeg)/The Living Tombstone/[Singles].bat
+++ Amazon Music Conversion (FFmpeg)/The Living Tombstone/[Singles].bat
@@ -1,19 +1,18 @@
 :: https://www.amazon.com/Love-I-Need-Living-Tombstone/dp/B08DWG65QS (Love I Need)
 
 :: Make new folder.
-mkdir "[Singles]"
+mkdir "[Singles] [Converted]"
 
 :: Copy cover art from first song, then resize it to 512x512 using a bicubic algorithim.
-ffmpeg -y -i "01 - Love I Need.mp3" -vf scale=512:512 -sws_flags bicubic cover_tmp.png
+ffmpeg -y -i "01 - Love I Need.mp3" -vf scale=480:480 -sws_flags bicubic cover_TMP.png
 
 :: Import song, add resized cover art, remove all current metadata, attach the song + cover art to the output file, register new metadata, initialize second part of adding cover art, encode audio into MP3, set sample rate to 44100 kHZ, set audio bitrate to 160kb, and output the new file.
-ffmpeg -y -i "01 - Love I Need.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata title="Love I Need" -metadata genre="Rock/Pop" -metadata language="English" -metadata copyright="© 2020, The Living Tombstone" -metadata track="1/1" -c:1 png -disposition:1 attached_pic -c:a libmp3lame -ar 44100 -b:a 160k "Love I Need.mp3"
+ffmpeg -i "01 - Love I Need.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata title="Love I Need"  -metadata track="1/1" -metadata genre="Electronic/Pop" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -movflags +faststart "Love I Need.m4a"
+move "Love I Need.mp3" "[Singles] [Converted]"
 
-:: Move new files to said directory.
-move "Love I Need.mp3" "%CD%\[Singles]"
 
 :: Stop the script, giving the user a chance to make sure everything looks good before proceeding to quit.
 pause
 
-:: Delete the temporary cover art file.
+:: Cleanup.
 DEL cover_tmp.png
