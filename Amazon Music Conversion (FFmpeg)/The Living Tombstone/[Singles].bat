00d492303a82fd48966a19752bf7682cd14608d6
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-10-10 19:36:26 -0700

    Update [Singles].bat
    Make correct version.

--- Amazon Music Conversion (FFmpeg)/The Living Tombstone/[Singles].bat
+++ Amazon Music Conversion (FFmpeg)/The Living Tombstone/[Singles].bat
@@ -7,7 +7,7 @@ mkdir "[Singles] [Converted]"
 ffmpeg -y -i "01 - Love I Need.mp3" -vf scale=480:480 -sws_flags bicubic cover_TMP.png
 
 :: Import song, add resized cover art, remove all current metadata, attach the song + cover art to the output file, register new metadata, initialize second part of adding cover art, encode audio into MP3, set sample rate to 44100 kHZ, set audio bitrate to 160kb, and output the new file.
-ffmpeg -i "01 - Love I Need.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata title="Love I Need"  -metadata track="1/1" -metadata genre="Electronic/Pop" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -af loudnorm=I=-16:TP=-2:LRA=8:linear=true:measured_I=-7.5:measured_LRA=2.5:measured_TP=0.8:measured_thresh=-17.7 -b:a 160k -movflags +faststart "Love I Need.m4a"
+ffmpeg -i "01 - Love I Need.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata title="Love I Need"  -metadata track="1/1" -metadata genre="Electronic/Pop" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Love I Need.m4a"
 move "Love I Need.m4a" "[Singles] [Converted]"
 
 :: Stop the script, giving the user a chance to make sure everything looks good before proceeding to quit.
