74c4c4598b1fb1734fee6216ed0414a673a6a8c1
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-10-17 15:23:36 -0700

    Create Spooky's Jump-Scare Mansion - HD Renovation.bat

---
+++ YouTube Conversion (youtube-dl+FFmpeg)/VineSauce - The Full Sauce/Spooky's Jump-Scare Mansion - HD Renovation.bat
@@ -0,0 +1,19 @@
+mkdir "Spooky's Jump-Scare Mansion - HD Renovation [Converted]" 
+chcp 65001 
+
+youtube-dl --embed-subs -f 135+140 https://www.youtube.com/watch?v=i8kwsw-Oi8w  
+ffmpeg -i "[Vinesauce] Vinny - Spooky's Jump Scare Mansion - HD Renovation (part 1)-i8kwsw-Oi8w.mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="VineSauce: The Full Sauce" -metadata show="Spooky's Jump-Scare Mansion - HD Renovation" -metadata title="Spooky's Jump-Scare Mansion - HD Renovation (Part 1)" -metadata track="1/3" -metadata genre="Horror" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Spooky's Jump-Scare Mansion - HD Renovation (Part 1).mp4"  
+move "Spooky's Jump-Scare Mansion - HD Renovation (Part 1).mp4" "Spooky's Jumpscare Mansion - HD Renovation [Converted]"  
+DEL "[Vinesauce] Vinny - Spooky's Jump Scare Mansion - HD Renovation (part 1)-i8kwsw-Oi8w.mp4"  
+
+youtube-dl --embed-subs -f 135+140 https://www.youtube.com/watch?v=MMT5jc_M9TY  
+ffmpeg -i "[Vinesauce] Vinny - Spooky's Jump Scare Mansion - HD Renovation (part 2)-MMT5jc_M9TY.mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="VineSauce: The Full Sauce" -metadata show="Spooky's Jump-Scare Mansion - HD Renovation" -metadata title="Spooky's Jump-Scare Mansion - HD Renovation (Part 2)" -metadata track="2/3" -metadata genre="Horror" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Spooky's Jump-Scare Mansion - HD Renovation (Part 2).mp4"  
+move "Spooky's Jump-Scare Mansion - HD Renovation (Part 2).mp4" "Spooky's Jumpscare Mansion - HD Renovation [Converted]"  
+DEL "[Vinesauce] Vinny - Spooky's Jump Scare Mansion - HD Renovation (part 2)-MMT5jc_M9TY.mp4"  
+
+youtube-dl --embed-subs -f 135+140 https://www.youtube.com/watch?v=3Ry7j6a2S0Q  
+ffmpeg -i "[Vinesauce] Vinny - Spooky's Jump Scare Mansion - HD Renovation (part 3 Finale)-3Ry7j6a2S0Q.mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="VineSauce: The Full Sauce" -metadata show="Spooky's Jump-Scare Mansion - HD Renovation" -metadata title="Spooky's Jump-Scare Mansion - HD Renovation (Part 3)" -metadata track="3/3" -metadata genre="Horror" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Spooky's Jump-Scare Mansion - HD Renovation (Part 3).mp4"  
+move "Spooky's Jump-Scare Mansion - HD Renovation (Part 3).mp4" "Spooky's Jumpscare Mansion - HD Renovation [Converted]"  
+DEL "[Vinesauce] Vinny - Spooky's Jump Scare Mansion - HD Renovation (part 3 Finale)-3Ry7j6a2S0Q.mp4"  
+
+pause 
