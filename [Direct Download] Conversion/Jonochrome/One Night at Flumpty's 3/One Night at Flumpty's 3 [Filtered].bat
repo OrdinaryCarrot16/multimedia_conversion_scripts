04413a02264599f4897af4d83353029169890915
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-11-15 10:45:42 -0800

    Create One Night at Flumpty's 3 [Filtered].bat
    W.I.P.

---
+++ [Direct Download] Conversion/Jonochrome/One Night at Flumpty's 3/One Night at Flumpty's 3 [Filtered].bat
@@ -0,0 +1,39 @@
+:: https://gamejolt.com/games/onaf3/529078 (Free)
+
+mkdir "One Night at Flumpty's [Soundtrack] [Converted]" 
+chcp 65001 
+
+ffmpeg -i "onaf3---01---main-menu.mp3" -map_metadata -1 -map 0:0 -id3v2_version 3 -metadata artist="Jonochrome" -metadata album="One Night at Flumpty's [Soundtrack]" -metadata title="Main Menu" -metadata track="1/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:a aac -aac_coder fast -ar 44100 -b:a 128k -movflags +faststart "Main Menu.m4a"  
+move "Main Menu.m4a" "One Night at Flumpty's [Soundtrack] [Converted]"  
+
+ffmpeg -i "onaf3---02---the-intercom.mp3" -map_metadata -1 -map 0:0 -id3v2_version 3 -metadata artist="Jonochrome" -metadata album="One Night at Flumpty's [Soundtrack]" -metadata title="The Intercom" -metadata track="2/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:a aac -aac_coder fast -ar 44100 -b:a 128k -movflags +faststart "The Intercom.m4a"  
+move "The Intercom.m4a" "One Night at Flumpty's [Soundtrack] [Converted]"  
+
+ffmpeg -i "onaf3---03---once-eggain.mp3" -map_metadata -1 -map 0:0 -id3v2_version 3 -metadata artist="Jonochrome" -metadata album="One Night at Flumpty's [Soundtrack]" -metadata title="Once Egg-ain" -metadata track="3/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:a aac -aac_coder fast -ar 44100 -b:a 128k -movflags +faststart "Once Egg-ain.m4a"  
+move "Once Egg-ain.m4a" "One Night at Flumpty's [Soundtrack] [Converted]"  
+
+ffmpeg -i "onaf3---04---eggs-and-beckon.mp3" -map_metadata -1 -map 0:0 -id3v2_version 3 -metadata artist="Jonochrome" -metadata album="One Night at Flumpty's [Soundtrack]" -metadata title="Eggs and Beckon" -metadata track="4/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:a aac -aac_coder fast -ar 44100 -b:a 128k -movflags +faststart "Eggs and Beckon.m4a"  
+move "Eggs and Beckon.m4a" "One Night at Flumpty's [Soundtrack] [Converted]"  
+
+ffmpeg -i "onaf3---05---eleggtroswing.mp3" -map_metadata -1 -map 0:0 -id3v2_version 3 -metadata artist="Jonochrome" -metadata album="One Night at Flumpty's [Soundtrack]" -metadata title="El-egg-troswing" -metadata track="5/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:a aac -aac_coder fast -ar 44100 -b:a 128k -movflags +faststart "El-egg-troswing.m4a"  
+move "El-egg-troswing.m4a" "One Night at Flumpty's [Soundtrack] [Converted]"  
+
+ffmpeg -i "onaf3---06---salami.mp3" -map_metadata -1 -map 0:0 -id3v2_version 3 -metadata artist="Jonochrome" -metadata album="One Night at Flumpty's [Soundtrack]" -metadata title="Salami" -metadata track="6/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:a aac -aac_coder fast -ar 44100 -b:a 128k -movflags +faststart "Salami.m4a"  
+move "Salami.m4a" "One Night at Flumpty's [Soundtrack] [Converted]"  
+
+ffmpeg -i "onaf3---07---the-eggnd.mp3" -map_metadata -1 -map 0:0 -id3v2_version 3 -metadata artist="Jonochrome" -metadata album="One Night at Flumpty's [Soundtrack]" -metadata title="The Egg-nd" -metadata track="7/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:a aac -aac_coder fast -ar 44100 -b:a 128k -movflags +faststart "The Egg-nd.m4a"  
+move "The Egg-nd.m4a" "One Night at Flumpty's [Soundtrack] [Converted]"  
+
+ffmpeg -i "onaf3---08---schwanenei.mp3" -map_metadata -1 -map 0:0 -id3v2_version 3 -metadata artist="Jonochrome" -metadata album="One Night at Flumpty's [Soundtrack]" -metadata title="Schwanenei" -metadata track="8/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:a aac -aac_coder fast -ar 44100 -b:a 128k -movflags +faststart "Schwanenei.m4a"  
+move "Schwanenei.m4a" "One Night at Flumpty's [Soundtrack] [Converted]"  
+
+ffmpeg -i "onaf3---09---6-am.mp3" -map_metadata -1 -map 0:0 -id3v2_version 3 -metadata artist="Jonochrome" -metadata album="One Night at Flumpty's [Soundtrack]" -metadata title="6 AM" -metadata track="9/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:a aac -aac_coder fast -ar 44100 -b:a 128k -movflags +faststart "6 AM.m4a"  
+move "6 AM.m4a" "One Night at Flumpty's [Soundtrack] [Converted]"  
+
+ffmpeg -i "onaf3---10---the-end.mp3" -map_metadata -1 -map 0:0 -id3v2_version 3 -metadata artist="Jonochrome" -metadata album="One Night at Flumpty's [Soundtrack]" -metadata title="The End" -metadata track="10/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:a aac -aac_coder fast -ar 44100 -b:a 128k -movflags +faststart "The End.m4a"  
+move "The End.m4a" "One Night at Flumpty's [Soundtrack] [Converted]"  
+
+ffmpeg -i "onaf3---11---promotional-trailer.mp3" -map_metadata -1 -map 0:0 -id3v2_version 3 -metadata artist="Jonochrome" -metadata album="One Night at Flumpty's [Soundtrack]" -metadata title="Promotional Trailer" -metadata track="11/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:a aac -aac_coder fast -ar 44100 -b:a 128k -movflags +faststart "Promotional Trailer.m4a"  
+move "Promotional Trailer.m4a" "One Night at Flumpty's [Soundtrack] [Converted]"  
+
+pause 
