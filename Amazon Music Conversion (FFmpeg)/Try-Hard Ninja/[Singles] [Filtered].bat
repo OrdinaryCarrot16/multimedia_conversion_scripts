54f101d4c834dd29d3d2f51d25c8fb649f1bcce4
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-11-16 11:24:47 -0800

    Update [Singles] [Filtered].bat

--- Amazon Music Conversion (FFmpeg)/Try-Hard Ninja/[Singles] [Filtered].bat
+++ Amazon Music Conversion (FFmpeg)/Try-Hard Ninja/[Singles] [Filtered].bat
@@ -2,7 +2,7 @@ mkdir "[Singles] [Converted]"
 chcp 65001 
 
 ffmpeg -y -i "01 - Judgement.mp3" -an -vf scale=480:480 -sws_flags bicubic cover_TMP.png  
-ffmpeg -i "01 - Judgement.mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="Try-Hard Ninja" -metadata title="Judgement" -metadata track="1/1" -metadata genre="Pop-Rock" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Judgement.m4a"  
+ffmpeg -i "01 - Judgement.mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="Try-Hard Ninja" -metadata title="Judgement" -metadata track="1/1" -metadata genre="Pop-Rock" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -af loudnorm=I=-14:TP=-2:LRA=8:linear=true:measured_I=-7.5:measured_LRA=8.1:measured_TP=0.5:measured_thresh=-17.7 -b:a 160k -movflags +faststart "Judgement.m4a"  
 move "Judgement.m4a" "[Singles] [Converted]"  
 
 pause 
