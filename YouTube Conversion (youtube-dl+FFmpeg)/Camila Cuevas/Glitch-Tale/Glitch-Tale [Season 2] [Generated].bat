d781901103f01ea86a757fcfb5cd1bb01b2ebc40
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-12-06 11:53:34 -0800

    Update Glitch-Tale [Season 2] [Generated].bat
    Title re-numbering (to compensate for 'parts' of episodes).

--- YouTube Conversion (youtube-dl+FFmpeg)/Camila Cuevas/Glitch-Tale/Glitch-Tale [Season 2] [Generated].bat
+++ YouTube Conversion (youtube-dl+FFmpeg)/Camila Cuevas/Glitch-Tale/Glitch-Tale [Season 2] [Generated].bat
@@ -2,57 +2,57 @@ mkdir "Glitch-Tale [Converted]"
 chcp 65001 
 
 yt-dlp --embed-subs -f 135+140 https://www.youtube.com/watch?v=DUdm30XHWVU  
-ffmpeg -i "My Sunshine - Glitchtale Season 2 (Ep #1) Undertale Animation [DUdm30XHWVU].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="My Sunshine" -metadata track="1/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "My Sunshine.mp4"  
+ffmpeg -i "My Sunshine - Glitchtale Season 2 (Ep #1) Undertale Animation [DUdm30XHWVU].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="My Sunshine" -metadata track="1/9" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "My Sunshine.mp4"  
 move "My Sunshine.mp4" "Glitch-Tale [Converted]"  
 DEL "My Sunshine - Glitchtale Season 2 (Ep #1) Undertale Animation [DUdm30XHWVU].mp4"  
 
 yt-dlp --embed-subs -f 135+140 https://www.youtube.com/watch?v=W82A03JTaqQ  
-ffmpeg -i "Dust - Glitchtale S2 Ep #2 (Undertale Animation) [W82A03JTaqQ].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="Dust" -metadata track="2/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Dust.mp4"  
+ffmpeg -i "Dust - Glitchtale S2 Ep #2 (Undertale Animation) [W82A03JTaqQ].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="Dust" -metadata track="2/9" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Dust.mp4"  
 move "Dust.mp4" "Glitch-Tale [Converted]"  
 DEL "Dust - Glitchtale S2 Ep #2 (Undertale Animation) [W82A03JTaqQ].mp4"  
 
 yt-dlp --embed-subs -f 135+140 https://www.youtube.com/watch?v=ANF6L7tIkd0  
-ffmpeg -i "Do or Die - Glitchtale S2 Ep #3 (Undertale Animation) [ANF6L7tIkd0].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="Do or Die" -metadata track="3/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Do or Die.mp4"  
+ffmpeg -i "Do or Die - Glitchtale S2 Ep #3 (Undertale Animation) [ANF6L7tIkd0].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="Do or Die" -metadata track="3/9" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Do or Die.mp4"  
 move "Do or Die.mp4" "Glitch-Tale [Converted]"  
 DEL "Do or Die - Glitchtale S2 Ep #3 (Undertale Animation) [ANF6L7tIkd0].mp4"  
 
 yt-dlp --embed-subs -f 135+140 https://www.youtube.com/watch?v=Yq0Ky_3pD-Q  
-ffmpeg -i "LOVE - Glitchtale S2 Ep #4 (Part 1) (Undertale Animation) [Yq0Ky_3pD-Q].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="LOVE" -metadata track="4/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "LOVE.mp4"  
+ffmpeg -i "LOVE - Glitchtale S2 Ep #4 (Part 1) (Undertale Animation) [Yq0Ky_3pD-Q].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="LOVE" -metadata track="4/9" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "LOVE.mp4"  
 move "LOVE.mp4" "Glitch-Tale [Converted]"  
 DEL "LOVE - Glitchtale S2 Ep #4 (Part 1) (Undertale Animation) [Yq0Ky_3pD-Q].mp4"  
 
 yt-dlp --embed-subs -f 135+140 https://www.youtube.com/watch?v=CUkYEJcGhIo  
-ffmpeg -i "Love - Glitchtale S2 Ep #4 (Part 2) (Undertale Animation) [CUkYEJcGhIo].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="LOVE [Part 2]" -metadata track="5/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "LOVE [Part 2].mp4"  
+ffmpeg -i "Love - Glitchtale S2 Ep #4 (Part 2) (Undertale Animation) [CUkYEJcGhIo].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="LOVE [Part 2]" -metadata track="4/9" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "LOVE [Part 2].mp4"  
 move "LOVE [Part 2].mp4" "Glitch-Tale [Converted]"  
 DEL "Love - Glitchtale S2 Ep #4 (Part 2) (Undertale Animation) [CUkYEJcGhIo].mp4"  
 
 yt-dlp --embed-subs -f 135+140 https://www.youtube.com/watch?v=WKEGJQWx5yY  
-ffmpeg -i "My Promise - Glitchtale S2 Ep #5 [WKEGJQWx5yY].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="My Promise" -metadata track="6/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "My Promise.mp4"  
+ffmpeg -i "My Promise - Glitchtale S2 Ep #5 [WKEGJQWx5yY].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="My Promise" -metadata track="5/9" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "My Promise.mp4"  
 move "My Promise.mp4" "Glitch-Tale [Converted]"  
 DEL "My Promise - Glitchtale S2 Ep #5 [WKEGJQWx5yY].mp4"  
 
 yt-dlp --embed-subs -f 135+140 https://www.youtube.com/watch?v=PNeSUiBhpUA  
-ffmpeg -i "Game Over  (Part 1) - Glitchtale S2 EP#6 _ ANIMATION [PNeSUiBhpUA].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="Game-Over" -metadata track="7/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Game-Over.mp4"  
+ffmpeg -i "Game Over  (Part 1) - Glitchtale S2 EP#6 _ ANIMATION [PNeSUiBhpUA].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="Game-Over" -metadata track="6/9" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Game-Over.mp4"  
 move "Game-Over.mp4" "Glitch-Tale [Converted]"  
 DEL "Game Over  (Part 1) - Glitchtale S2 EP#6 _ ANIMATION [PNeSUiBhpUA].mp4"  
 
 yt-dlp --embed-subs -f 135+140 https://www.youtube.com/watch?v=8hMgb4pGE9U  
-ffmpeg -i "Game Over (Part 2) - Glitchtale S2 EP#6 _ ANIMATION [8hMgb4pGE9U].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="Game-Over [Part 2]" -metadata track="8/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Game-Over [Part 2].mp4"  
+ffmpeg -i "Game Over (Part 2) - Glitchtale S2 EP#6 _ ANIMATION [8hMgb4pGE9U].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="Game-Over [Part 2]" -metadata track="6/9" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Game-Over [Part 2].mp4"  
 move "Game-Over [Part 2].mp4" "Glitch-Tale [Converted]"  
 DEL "Game Over (Part 2) - Glitchtale S2 EP#6 _ ANIMATION [8hMgb4pGE9U].mp4"  
 
 yt-dlp --embed-subs -f 135+140 https://www.youtube.com/watch?v=yVZKjHh4Qj0  
-ffmpeg -i "H???a???t???e??? - Glitchtale S2 EP #7 _ ANIMATION [yVZKjHh4Qj0].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="HATE" -metadata track="9/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "HATE.mp4"  
+ffmpeg -i "H???a???t???e??? - Glitchtale S2 EP #7 _ ANIMATION [yVZKjHh4Qj0].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="HATE" -metadata track="7/9" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "HATE.mp4"  
 move "HATE.mp4" "Glitch-Tale [Converted]"  
 DEL "H???a???t???e??? - Glitchtale S2 EP #7 _ ANIMATION [yVZKjHh4Qj0].mp4"  
 
 yt-dlp --embed-subs -f 135+140 https://www.youtube.com/watch?v=nRZ4Qs_d11o  
-ffmpeg -i "Animosity - Glitchtale S2 EP #8 _ ANIMATION [nRZ4Qs_d11o].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="Animosity" -metadata track="10/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Animosity.mp4"  
+ffmpeg -i "Animosity - Glitchtale S2 EP #8 _ ANIMATION [nRZ4Qs_d11o].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="Animosity" -metadata track="8/9" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Animosity.mp4"  
 move "Animosity.mp4" "Glitch-Tale [Converted]"  
 DEL "Animosity - Glitchtale S2 EP #8 _ ANIMATION [nRZ4Qs_d11o].mp4"  
 
 yt-dlp --embed-subs -f 135+140 https://www.youtube.com/watch?v=kniGbjJqfTQ  
-ffmpeg -i "Hope - Glitchtale S2 EP #9 _ Season Finale _ ANIMATION [kniGbjJqfTQ].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="Hope" -metadata track="11/11" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Hope.mp4"  
+ffmpeg -i "Hope - Glitchtale S2 EP #9 _ Season Finale _ ANIMATION [kniGbjJqfTQ].mp4" -map_metadata -1 -map_chapters -1 -map 0:0 -map 0:1 -id3v2_version 3 -metadata artist="Camila Cuevas" -metadata show="Glitch-Tale" -metadata title="Hope" -metadata track="9/9" -metadata genre="" -metadata language="English" -metadata encoder="FFmpeg (libx264 + aac)" -threads 2 -c:a copy -c:v copy -movflags +faststart "Hope.mp4"  
 move "Hope.mp4" "Glitch-Tale [Converted]"  
 DEL "Hope - Glitchtale S2 EP #9 _ Season Finale _ ANIMATION [kniGbjJqfTQ].mp4"  
 
