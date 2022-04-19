736fff3cd328658886ff6f27866a25215ae99c94
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2022-04-18 23:44:53 -0700

    Create [Remixes].bat

---
+++ Amazon Music Conversion (FFmpeg)/The Living Tombstone/[Remixes].bat
@@ -0,0 +1,12 @@
+:: https://www.amazon.com/dp/B076J68YH6 (ECHO)
+
+mkdir "[Remixes] [Converted]" 
+chcp 65001 
+
+ffmpeg -y -i "01 - Echo (feat. Crusher-P).mp3" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png  
+ffmpeg -i "01 - Echo (feat. Crusher-P).mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata title="ECHO" -metadata genre="Dubstep" -metadata language="eng" -metadata date="2017-05-31" -metadata comment="Original song 'ECHO' by 'Crusher-P'." -c:1 png -disposition:1 attached_pic -af "loudnorm=I=-18:TP=-2:LRA=8:linear=true:measured_I=-6.3:measured_LRA=5.4:measured_TP=1.4:measured_thresh=-16.6" -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "ECHO.m4a"  
+move "ECHO.m4a" "[Remixes] [Converted]"  
+
+pause
+
+del cover_TMP.png 
