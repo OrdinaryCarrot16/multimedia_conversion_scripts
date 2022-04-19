88b2881f3f709def3e28af0347332cf65a3cc1ea
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2022-04-18 21:03:10 -0700

    Update [Singles].bat
    Source

--- Amazon Music Conversion (FFmpeg)/The Living Tombstone/[Singles].bat
+++ Amazon Music Conversion (FFmpeg)/The Living Tombstone/[Singles].bat
@@ -1,17 +1,28 @@
-:: https://www.amazon.com/Love-I-Need-Living-Tombstone/dp/B08DWG65QS (Love I Need)
+:: https://www.amazon.com/dp/B09TY7NMVL (Hit The Snooze)
+:: https://www.amazon.com/dp/B086R97Y1Q (I Can't Fix You)
+:: https://www.amazon.com/dp/B08DWG65QS (Love I Need)
+:: https://www.amazon.com/dp/B09R3T568F (Temporary Love)
 
-:: Make new folder.
-mkdir "[Singles] [Converted]"
 
-:: Copy cover art from first song, then resize it to 512x512 using a bicubic algorithim.
-ffmpeg -y -i "01 - Love I Need.mp3" -vf scale=480:480 -sws_flags bicubic cover_TMP.png
+mkdir "[Singles] [Converted]" 
+chcp 65001 
 
-:: Import song, add resized cover art, remove all current metadata, attach the song + cover art to the output file, register new metadata, initialize second part of adding cover art, encode audio into MP3, set sample rate to 44100 kHZ, set audio bitrate to 160kb, and output the new file.
-ffmpeg -i "01 - Love I Need.mp3" -i cover_tmp.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata title="Love I Need"  -metadata track="1/1" -metadata genre="Electronic/Pop" -metadata language="English" -metadata encoder="FFmpeg (aac)" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Love I Need.m4a"
-move "Love I Need.m4a" "[Singles] [Converted]"
+ffmpeg -y -i "01 - Hit The Snooze [Explicit].mp3" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png  
+ffmpeg -i "01 - Hit The Snooze [Explicit].mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata title="Hit the Snooze" -metadata genre="Electronic/Pop" -metadata language="eng" -metadata date="2022-03-11" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Hit the Snooze.m4a"  
+move "Hit the Snooze.m4a" "[Singles] [Converted]"  
 
-:: Stop the script, giving the user a chance to make sure everything looks good before proceeding to quit.
-pause
+ffmpeg -y -i "01 - I Can't Fix You (feat. Crusher-P).mp3" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png  
+ffmpeg -i "01 - I Can't Fix You (feat. Crusher-P).mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone;Crusher-P" -metadata title="I Can't Fix You" -metadata genre="Electronic/Pop" -metadata language="eng" -metadata date="2016-12-10" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "I Can't Fix You.m4a"  
+move "I Can't Fix You.m4a" "[Singles] [Converted]"  
 
-:: Cleanup.
-DEL cover_tmp.png
+ffmpeg -y -i "01 - Love I Need.mp3" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png  
+ffmpeg -i "01 - Love I Need.mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata title="Love I Need" -metadata genre="Electronic/Pop" -metadata language="eng" -metadata date="2020-08-06" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Love I Need.m4a"  
+move "Love I Need.m4a" "[Singles] [Converted]"  
+
+ffmpeg -y -i "01 - Temporary Love (feat. CG5).mp3" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png  
+ffmpeg -i "01 - Temporary Love (feat. CG5).mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone;CG5" -metadata title="Temporary Love" -metadata genre="Electronic/Pop" -metadata language="eng" -metadata date="2022-01-28" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Temporary Love.m4a"  
+move "Temporary Love.m4a" "[Singles] [Converted]"  
+
+pause 
+
+DEL cover_TMP.png 
