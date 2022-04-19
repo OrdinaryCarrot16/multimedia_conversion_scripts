8f27e51ec8ae2f2dd71bfd1a286b4900471294c5
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2022-04-18 22:04:25 -0700

    Update [Remixes].bat

--- [Direct Download] Conversion/The Living Tombstone/[Remixes].bat
+++ [Direct Download] Conversion/The Living Tombstone/[Remixes].bat
@@ -1,5 +1,5 @@
 :: https://www.mediafire.com/file/ggto9rcz8yew2mc/TombAbsoluteTerritoryRemix-Final.mp3/file (Absolute Territory)
-:: https://www.mediafire.com/file/a28veehw21gq6dc/TombstoneDiscordRemix-Final.mp3/file (Discord [Remix])
+:: https://www.mediafire.com/file/a28veehw21gq6dc/TombstoneDiscordRemix-Final.mp3/file (Discord)
 
 mkdir "[Remixes] [Converted]" 
 chcp 65001 
