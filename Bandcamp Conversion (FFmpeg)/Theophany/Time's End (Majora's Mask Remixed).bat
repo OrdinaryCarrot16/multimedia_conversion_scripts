4f6eb733393ae2dbcc4aae0648ab378951986107
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2022-04-19 20:12:54 -0700

    Update Time's End (Majora's Mask Remixed).bat

--- Bandcamp Conversion (FFmpeg)/Theophany/Time's End (Majora's Mask Remixed).bat
+++ Bandcamp Conversion (FFmpeg)/Theophany/Time's End (Majora's Mask Remixed).bat
@@ -4,7 +4,7 @@ chcp 65001
 ffmpeg -i "cover.png" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png 
 
 ffmpeg -i "Theophany - Time's End- Majora's Mask Remixed - 01 Majora's Mask.mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="Theophany" -metadata album_artist="Theophany" -metadata album="Time's End (Majora's Mask Remixed)" -metadata title="Majora's Mask" -metadata track="1/10" -metadata genre="Orchestral" -metadata language="" -metadata date="2012-12-21" -c:1 png -disposition:1 attached_pic -af "loudnorm=I=-18:TP=-2:LRA=8:linear=true:measured_I=-10.1:measured_LRA=12.5:measured_TP=0.4:measured_thresh=-20.7" -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Majora's Mask.m4a"  
-move "Majora's Mask.m4a" "Time's End - Majora's Mask Remixed [Converted]"  
+move "Majora's Mask.m4a" "Time's End (Majora's Mask Remixed) [Converted]"  
 
 ffmpeg -i "Theophany - Time's End- Majora's Mask Remixed - 02 The Clockworks.mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="Theophany" -metadata album_artist="Theophany" -metadata album="Time's End (Majora's Mask Remixed)" -metadata title="The Clock-Works" -metadata track="2/10" -metadata genre="Orchestral" -metadata language="" -metadata date="2012-12-21" -c:1 png -disposition:1 attached_pic -af "loudnorm=I=-18:TP=-2:LRA=8:linear=true:measured_I=-13.2:measured_LRA=12.2:measured_TP=0:measured_thresh=-23.9" -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "The Clock-Works.m4a"  
 move "The Clock-Works.m4a" "Time's End (Majora's Mask Remixed) [Converted]"  
