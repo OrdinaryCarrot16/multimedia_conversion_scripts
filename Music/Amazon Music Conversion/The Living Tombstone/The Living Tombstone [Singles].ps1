64a3ad400b92c533ac41cb9b493815fed9c9cf5a
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2022-05-17 10:44:34 -0700

    Update The Living Tombstone [Singles].ps1
    Genres updated

--- Music/Amazon Music Conversion/The Living Tombstone/The Living Tombstone [Singles].ps1
+++ Music/Amazon Music Conversion/The Living Tombstone/The Living Tombstone [Singles].ps1
@@ -14,11 +14,11 @@ ffmpeg -i "01 - 1000 Doors (Spooky's Jumpscare Mansion Song) [feat. Bobby Yarsul
 Move-Item -Path "1000 Doors.m4a" -Destination "The Living Tombstone/[Singles] [Converted]"
 
 ffmpeg -y -i "01 - Beep Beep I'm a Sheep (feat. LilDeuceDeuce, TomSka & BlackGryph0n).mp3" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png
-ffmpeg -i "01 - Beep Beep I'm a Sheep (feat. LilDeuceDeuce, TomSka & BlackGryph0n).mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata album_artist="The Living Tombstone" -metadata album="Beep Beep I'm a Sheep [Single] [Remix]" -metadata date="2017-04-26" -metadata genre="EDM" -metadata title="Beep Beep I'm a Sheep" -metadata comment="Remix of the song 'Beep Beep I'm a Sheep' by 'LilDeuceDeuce'." -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Beep Beep I'm a Sheep.m4a"
+ffmpeg -i "01 - Beep Beep I'm a Sheep (feat. LilDeuceDeuce, TomSka & BlackGryph0n).mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata album_artist="The Living Tombstone" -metadata album="Beep Beep I'm a Sheep [Single] [Remix]" -metadata date="2017-04-26" -metadata genre="Dub-Step | EDM" -metadata title="Beep Beep I'm a Sheep" -metadata comment="Remix of the song 'Beep Beep I'm a Sheep' by 'LilDeuceDeuce'." -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Beep Beep I'm a Sheep.m4a"
 Move-Item -Path "Beep Beep I'm a Sheep.m4a" -Destination "The Living Tombstone/[Singles] [Converted]"
 
 ffmpeg -y -i "01 - Cut the Cord.mp3" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png
-ffmpeg -i "01 - Cut the Cord.mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone / EileMonty" -metadata album_artist="The Living Tombstone" -metadata album="Cut the Cord [Single]" -metadata date="2017-02-01" -metadata genre="Pop" -metadata title="Cut The Cord" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Cut The Cord.m4a"
+ffmpeg -i "01 - Cut the Cord.mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone / EileMonty" -metadata album_artist="The Living Tombstone" -metadata album="Cut the Cord [Single]" -metadata date="2017-02-01" -metadata genre="Electronic | Pop" -metadata title="Cut The Cord" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Cut The Cord.m4a"
 Move-Item -Path "Cut The Cord.m4a" -Destination "The Living Tombstone/[Singles] [Converted]"
 
 ffmpeg -y -i "01 - Echo (feat. Crusher-P).mp3" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png
@@ -26,7 +26,7 @@ ffmpeg -i "01 - Echo (feat. Crusher-P).mp3" -i cover_TMP.png -map_metadata -1 -m
 Move-Item -Path "ECHO.m4a" -Destination "The Living Tombstone/[Singles] [Converted]"
 
 ffmpeg -y -i "01 - Hit The Snooze [Explicit].mp3" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png
-ffmpeg -i "01 - Hit The Snooze [Explicit].mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata album_artist="The Living Tombstone" -metadata album="Hit The Snooze [Single]" -metadata date="2022-03-11" -metadata genre="Pop" -metadata title="Hit The Snooze" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Hit The Snooze.m4a"
+ffmpeg -i "01 - Hit The Snooze [Explicit].mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata album_artist="The Living Tombstone" -metadata album="Hit The Snooze [Single]" -metadata date="2022-03-11" -metadata genre="Electronic | Pop" -metadata title="Hit The Snooze" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Hit The Snooze.m4a"
 Move-Item -Path "Hit The Snooze.m4a" -Destination "The Living Tombstone/[Singles] [Converted]"
 
 ffmpeg -y -i "01 - I Can't Fix You (feat. Crusher-P).mp3" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png
@@ -34,7 +34,7 @@ ffmpeg -i "01 - I Can't Fix You (feat. Crusher-P).mp3" -i cover_TMP.png -map_met
 Move-Item -Path "I Can't Fix You.m4a" -Destination "The Living Tombstone/[Singles] [Converted]"
 
 ffmpeg -y -i "01 - Temporary Love (feat. CG5).mp3" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png
-ffmpeg -i "01 - Temporary Love (feat. CG5).mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata album_artist="The Living Tombstone / CG5" -metadata album="Temporary Love [Single]" -metadata date="2022-01-28" -metadata genre="Pop" -metadata title="Temporary Love" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Temporary Love.m4a"
+ffmpeg -i "01 - Temporary Love (feat. CG5).mp3" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist="The Living Tombstone" -metadata album_artist="The Living Tombstone / CG5" -metadata album="Temporary Love [Single]" -metadata date="2022-01-28" -metadata genre="Electronic | Pop" -metadata title="Temporary Love" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart "Temporary Love.m4a"
 Move-Item -Path "Temporary Love.m4a" -Destination "The Living Tombstone/[Singles] [Converted]"
 
 
