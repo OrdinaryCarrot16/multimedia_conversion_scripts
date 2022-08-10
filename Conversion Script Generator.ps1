e59937119c36120d016cddd2dab479a0e8922caa
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2022-08-10 14:41:11 -0700

    Update Conversion Script Generator.ps1
    Ogg-ification! Still working on getting covers to work for singles.

--- Conversion Script Generator.ps1
+++ Conversion Script Generator.ps1
@@ -15,16 +15,16 @@ if ($album_or_single -eq 0) {
 	[int]$track_amount=Read-Host -Prompt "How many Tracks/Songs are in this Album"
 		New-Item "$artist - $album [Generated].ps1" -ItemType File -Force
 		Add-Content -LiteralPath "$artist - $album [Generated].ps1" -Value "New-Item -Path `"$artist/$album [Converted]`" -ItemType Directory`n`n"
-		Add-Content -LiteralPath "$artist - $album [Generated].ps1" -Value "ffmpeg -i `"$cover_art`" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png`n`n"
-		Get-ChildItem "*.mp3"
+		Add-Content -LiteralPath "$artist - $album [Generated].ps1" -Value "ffmpeg -i `"$cover_art`" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png"
+		Add-Content -LiteralPath "$artist - $album [Generated].ps1" -Value "Move-Item -Path `"cover_TMP.png`" -Destination `"$artist/$album [Converted]/cover.png`"`n`n"
+		Get-ChildItem "*.*"
 			do {
 				$TRACK_COUNTER+=1
 				$filename=Read-Host -Prompt "`nSong File Name ($TRACK_COUNTER/$track_amount)"
 				$songname=Read-Host -Prompt "Name of the Song"
-					Add-Content -LiteralPath "$artist - $album [Generated].ps1" -Value "ffmpeg -i `"$filename`" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist=`"$artist`" -metadata album_artist=`"$artist`" -metadata album=`"$album`" -metadata date=`"$date`" -metadata genre=`"$genre`" -metadata track=`"$TRACK_COUNTER/$track_amount`" -metadata title=`"$songname`" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart `"$songname.m4a`""
-					Add-Content -LiteralPath "$artist - $album [Generated].ps1" -Value "Move-Item -Path `"$songname.m4a`" -Destination `"$artist/$album [Converted]`"`n"
+					Add-Content -LiteralPath "$artist - $album [Generated].ps1" -Value "ffmpeg -i `"$filename`" -map_metadata -1 -map 0:0 -id3v2_version 3 -metadata artist=`"$artist`" -metadata album_artist=`"$artist`" -metadata album=`"$album`" -metadata date=`"$date`" -metadata genre=`"$genre`" -metadata track=`"$TRACK_COUNTER/$track_amount`" -metadata title=`"$songname`" -c:a libvorbis -q 5 `"$songname.ogg`""
+					Add-Content -LiteralPath "$artist - $album [Generated].ps1" -Value "Move-Item -Path `"$songname.ogg`" -Destination `"$artist/$album [Converted]`"`n"
 			} until ($TRACK_COUNTER -eq $track_amount)
-	Add-Content -LiteralPath "$artist - $album [Generated].ps1" -Value "`nRemove-Item `"cover_TMP.png`"`n`n"
 	Add-Content -LiteralPath "$artist - $album [Generated].ps1" -Value "pause"
 }
 
@@ -33,20 +33,23 @@ if ($album_or_single -eq 1) {
 	[int]$track_amount=Read-Host -Prompt "How many seperate Single`'s are there"
 		New-Item "$artist [Singles] [Generated].ps1" -ItemType File -Force
 		Add-Content -LiteralPath "$artist [Singles] [Generated].ps1" -Value "New-Item -Path `"$artist/[Singles] [Converted]`" -ItemType Directory`n`n"
-		Get-ChildItem "*.mp3"
 		do {
 			$TRACK_COUNTER+=1
+			Get-ChildItem "*.*"
 			$filename=Read-Host -Prompt "`nSong File Name ($TRACK_COUNTER/$track_amount)"
 			$cover_art=Read-Host -Prompt "Select the audio/image file with the Cover Art"
-				Add-Content -LiteralPath "$artist [Singles] [Generated].ps1" -Value "ffmpeg -y -i `"$cover_art`" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png"
+				ffmpeg -y -i "$cover_art" -an -vf scale=512:512 -sws_flags bicubic cover_TMP.png
+				$base64=[convert]::ToBase64String((Get-Content cover_TMP.png -AsByteStream -Raw))
+				New-Item "ffmetadata_$TRACK_COUNTER.txt" -ItemType File -Force
+				Add-Content -LiteralPath "ffmetadata_$TRACK_COUNTER.txt" -Value ";FFMETADATA1`n`nMETADATA_BLOCK_PICTURE=$base64"
 			$songname=Read-Host -Prompt "Name of the Song"
 			$genre=Read-Host -Prompt "Genre of the Song"
 			$date=Read-Host -Prompt "Enter the release date of the single (in YYYY-MM-DD format, or type nothing if unknown)"
-				Add-Content -LiteralPath "$artist [Singles] [Generated].ps1" -Value "ffmpeg -i `"$filename`" -i cover_TMP.png -map_metadata -1 -map 0:0 -map 1:0 -id3v2_version 3 -metadata artist=`"$artist`" -metadata album_artist=`"$artist`" -metadata album=`"$songname [Single]`" -metadata date=`"$date`" -metadata genre=`"$genre`" -metadata title=`"$songname`" -c:1 png -disposition:1 attached_pic -c:a aac -aac_coder fast -ar 44100 -b:a 160k -movflags +faststart `"$songname.m4a`""
-				Add-Content -LiteralPath "$artist [Singles] [Generated].ps1" -Value "Move-Item -Path `"$songname.m4a`" -Destination `"$artist/[Singles] [Converted]`"`n"
+				Add-Content -LiteralPath "$artist [Singles] [Generated].ps1" -Value "ffmpeg -i `"$filename`" -i ffmetadata_$TRACK_COUNTER.txt -map_metadata -1 -map_metadata 1 -map 0:0 -id3v2_version 3 -metadata artist=`"$artist`" -metadata album_artist=`"$artist`" -metadata album=`"$songname [Single]`" -metadata date=`"$date`" -metadata genre=`"$genre`" -metadata title=`"$songname`" -c:a libvorbis -q 5 `"$songname.ogg`""
+				Add-Content -LiteralPath "$artist [Singles] [Generated].ps1" -Value "Move-Item -Path `"$songname.ogg`" -Destination `"$artist/[Singles] [Converted]`"`n"
 		} until ($TRACK_COUNTER -eq $track_amount)
-	Add-Content -LiteralPath "$artist [Singles] [Generated].ps1" -Value "`nRemove-Item `"cover_TMP.png`"`n`n"
-	Add-Content -LiteralPath "$artist [Singles] [Generated].ps1" -Value "pause"
+	Remove-Item "cover_TMP.png"
+	Add-Content -LiteralPath "$artist [Singles] [Generated].ps1" -Value "`npause"
 }
 
 pause
