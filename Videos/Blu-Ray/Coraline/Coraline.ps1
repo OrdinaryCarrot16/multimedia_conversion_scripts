dad92b0b3a07db5e85b0ee0471c37ecd4d8d8238
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2023-02-10 21:12:00 -0800

    Update Coraline.ps1

--- Videos/Blu-Ray/Coraline/Coraline.ps1
+++ Videos/Blu-Ray/Coraline/Coraline.ps1
@@ -1,3 +1,13 @@
 New-Item -Path "$home/Videos/MakeMKV/Coraline" -ItemType Directory
+New-Item -Path "$home/Videos/MakeMKV/Coraline/Extras" -ItemType Directory
 
-./"makemkvcon64" --minlength=15 mkv disc:0 06 
+# Movie
+./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 06 "$home/Videos/MakeMKV/Coraline"
+Rename-Item -Path "$home/Videos/MakeMKV/Coraline/title_t06.mkv" -NewName "Coraline.mkv"
+
+# Extra
+./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 03 "$home/Videos/MakeMKV/Coraline/Extras"
+Rename-Item -Path "$home/Videos/MakeMKV/Coraline/Extras/title_t03.mkv" -NewName "The Making of Coraline.mkv"
+
+./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 04 "$home/Videos/MakeMKV/Coraline/Extras"
+Rename-Item -Path "$home/Videos/MakeMKV/Coraline/Extras/title_t04.mkv" -NewName "Coraline Animatic.mkv"
