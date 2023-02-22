bc0e51cc10d10df50be68f0f86eb41e7359ebf7a
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2023-02-21 21:37:36 -0800

    Update Coraline.ps1

--- Videos/Blu-Ray/Coraline/Coraline.ps1
+++ Videos/Blu-Ray/Coraline/Coraline.ps1
@@ -2,47 +2,47 @@ New-Item -Path "MakeMKV/Coraline" -ItemType Directory
 New-Item -Path "MakeMKV/Coraline/Extras" -ItemType Directory
 
 # Movie
-./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 06 "MakeMKV/Coraline"
+makemkvcon64 --minlength=15 --progress=-same mkv disc:0 06 "MakeMKV/Coraline"
 Rename-Item -Path "MakeMKV/Coraline/Coraline_t06.mkv" -NewName "Coraline.mkv"
 
 # Extra
-./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 03 "MakeMKV/Coraline/Extras"
+makemkvcon64 --minlength=15 --progress=-same mkv disc:0 03 "MakeMKV/Coraline/Extras"
 Rename-Item -Path "MakeMKV/Coraline/Extras/Coraline_t03.mkv" -NewName "The Making of Coraline.mkv"
 
-./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 04 "MakeMKV/Coraline/Extras"
+makemkvcon64 --minlength=15 --progress=-same mkv disc:0 04 "MakeMKV/Coraline/Extras"
 Rename-Item -Path "MakeMKV/Coraline/Extras/Coraline_t04.mkv" -NewName "Coraline Animatic.mkv"
 
-./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 05 "MakeMKV/Coraline/Extras"
+makemkvcon64 --minlength=15 --progress=-same mkv disc:0 05 "MakeMKV/Coraline/Extras"
 Rename-Item -Path "MakeMKV/Coraline/Extras/Coraline_t05.mkv" -NewName "Deleted Scenes.mkv"
 
-./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 08 "MakeMKV/Coraline/Extras"
+makemkvcon64 --minlength=15 --progress=-same mkv disc:0 08 "MakeMKV/Coraline/Extras"
 Rename-Item -Path "MakeMKV/Coraline/Extras/Coraline_t08.mkv" -NewName "Inside Laika - The Cat.mkv"
 
-./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 09 "MakeMKV/Coraline/Extras"
+makemkvcon64 --minlength=15 --progress=-same mkv disc:0 09 "MakeMKV/Coraline/Extras"
 Rename-Item -Path "MakeMKV/Coraline/Extras/Coraline_t09.mkv" -NewName "Voices Of Coraline.mkv"
 
-./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 22 "MakeMKV/Coraline/Extras"
+makemkvcon64 --minlength=15 --progress=-same mkv disc:0 22 "MakeMKV/Coraline/Extras"
 Rename-Item -Path "MakeMKV/Coraline/Extras/Coraline_t22.mkv" -NewName "Inside Laika - Wybie Lovat.mkv"
 
-./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 23 "MakeMKV/Coraline/Extras"
+makemkvcon64 --minlength=15 --progress=-same mkv disc:0 23 "MakeMKV/Coraline/Extras"
 Rename-Item -Path "MakeMKV/Coraline/Extras/Coraline_t23.mkv" -NewName "Inside Laika - Other Mother.mkv"
 
-./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 24 "MakeMKV/Coraline/Extras"
+makemkvcon64 --minlength=15 --progress=-same mkv disc:0 24 "MakeMKV/Coraline/Extras"
 Rename-Item -Path "MakeMKV/Coraline/Extras/Coraline_t24.mkv" -NewName "Inside Laika - Other Father.mkv"
 
-./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 25 "MakeMKV/Coraline/Extras"
+makemkvcon64 --minlength=15 --progress=-same mkv disc:0 25 "MakeMKV/Coraline/Extras"
 Rename-Item -Path "MakeMKV/Coraline/Extras/Coraline_t25.mkv" -NewName "Inside Laika - Coraline Jones.mkv"
 
-./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 26 "MakeMKV/Coraline/Extras"
+makemkvcon64 --minlength=15 --progress=-same mkv disc:0 26 "MakeMKV/Coraline/Extras"
 Rename-Item -Path "MakeMKV/Coraline/Extras/Coraline_t26.mkv" -NewName "Inside Laika - Mr Bobinsky.mkv"
 
-./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 27 "MakeMKV/Coraline/Extras"
+makemkvcon64 --minlength=15 --progress=-same mkv disc:0 27 "MakeMKV/Coraline/Extras"
 Rename-Item -Path "MakeMKV/Coraline/Extras/Coraline_t27.mkv" -NewName "Inside Laika - The Beldam.mkv"
 
-./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 29 "MakeMKV/Coraline/Extras"
+makemkvcon64 --minlength=15 --progress=-same mkv disc:0 29 "MakeMKV/Coraline/Extras"
 Rename-Item -Path "MakeMKV/Coraline/Extras/Coraline_t29.mkv" -NewName "Horrors Of Coraline.mkv"
 
-./"makemkvcon64" --minlength=15 --progress=-same mkv disc:0 29 "MakeMKV/Coraline/Extras"
+makemkvcon64 --minlength=15 --progress=-same mkv disc:0 29 "MakeMKV/Coraline/Extras"
 Rename-Item -Path "MakeMKV/Coraline/Extras/Coraline_t29.mkv" -NewName "Stop-Motion.mkv"
 
 Write-Host "Push enter to proceed to transcoding, or close app to keep original files"
