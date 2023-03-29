92c566896654021b0bfc4cc99cad7e47e7af502f
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2023-03-29 13:33:30 -0700

    Update Conversion Script Generator.ps1
    Track number added to file name of songs.

--- Conversion Script Generator.ps1
+++ Conversion Script Generator.ps1
@@ -24,8 +24,8 @@ if ($media_type -eq "music") {
 				$TRACK_COUNTER+=1
 				$filename=Read-Host -Prompt "`nSong File Name ($TRACK_COUNTER/$track_amount)"
 				$songname=Read-Host -Prompt "Name of the Song"
-					Add-Content -LiteralPath "$artist - $album (Generated).ps1" -Value "ffmpeg -i `"$filename`" -map_metadata -1 -map 0:0 -metadata artist=`"$artist`" -metadata album_artist=`"$artist`" -metadata album=`"$album`" -metadata date=`"$date`" -metadata genre=`"$genre`" -metadata track=`"$TRACK_COUNTER/$track_amount`" -metadata title=`"$songname`" -c:a libvorbis -q 5 -minrate 128k `"$songname.ogg`""
-					Add-Content -LiteralPath "$artist - $album (Generated).ps1" -Value "Move-Item -Path `"$songname.ogg`" -Destination `"$artist/$album (Converted)`"`n"
+					Add-Content -LiteralPath "$artist - $album (Generated).ps1" -Value "ffmpeg -i `"$filename`" -map_metadata -1 -map 0:0 -metadata artist=`"$artist`" -metadata album_artist=`"$artist`" -metadata album=`"$album`" -metadata date=`"$date`" -metadata genre=`"$genre`" -metadata track=`"$TRACK_COUNTER/$track_amount`" -metadata title=`"$songname`" -c:a libvorbis -q 5 -minrate 128k `"$TRACK_COUNTER - $songname.ogg`""
+					Add-Content -LiteralPath "$artist - $album (Generated).ps1" -Value "Move-Item -Path `"$TRACK_COUNTER - $songname.ogg`" -Destination `"$artist/$album (Converted)`"`n"
 			} until ($TRACK_COUNTER -eq $track_amount)
 	Add-Content -LiteralPath "$artist - $album (Generated).ps1" -Value "pause"
 	}
