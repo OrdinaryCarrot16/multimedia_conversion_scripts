b65653e6db9ab87eef83acc38802da8fd59e0e87
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2023-04-29 13:50:30 -0700

    Update The Chalk-Eaters - (Singles).ps1

--- Music/Bandcamp/The Chalk-Eaters/The Chalk-Eaters - (Singles).ps1
+++ Music/Bandcamp/The Chalk-Eaters/The Chalk-Eaters - (Singles).ps1
@@ -1,8 +1,11 @@
+# https://thechalkeaters.bandcamp.com/album/furr-mon-gotta-smash-em-all (Furrymon - Gotta Smash 'em All)
+# https://thechalkeaters.bandcamp.com/track/crushing-thirties (Crushing Thirties)
+
 New-Item -Path "The Chalk-Eaters/(Singles) (Converted)" -ItemType Directory
 
 
-ffmpeg -y -i "The Chalkeaters feat. Black Gryph0n & PiNKII - Furrýmon- Gotta Smash ’Em All!.flac" -an -vf scale=1024:1024 -sws_flags bicubic "Furrymon - Gotta Smash Them All (thumb).png"
-ffmpeg -ss 00:00:00.400 -to 00:03:29.900 -i "The Chalkeaters feat. Black Gryph0n & PiNKII - Furrýmon- Gotta Smash ’Em All!.flac" -map_metadata -1 -map 0:0 -metadata artist="The Chalk-Eaters / Black Gryph0n / PiNKII" -metadata album_artist="The Chalk-Eaters" -metadata album="Furrymon - Gotta Smash Them All (Single)" -metadata date="2022-11-25" -metadata genre="Rock | Funk | Rap" -metadata title="Furrymon - Gotta Smash 'em All" -af "apad=pad_dur=3s,volume=-5dB" -c:a libvorbis -q 6 -minrate 128k "Furrymon - Gotta Smash Them All.ogg"
+ffmpeg -y -i "The Chalkeaters feat. Black Gryph0n & PiNKII - FurrÃ½mon- Gotta Smash â€™Em All!.flac" -an -vf scale=1024:1024 -sws_flags bicubic "Furrymon - Gotta Smash Them All (thumb).png"
+ffmpeg -ss 00:00:00.400 -to 00:03:29.900 -i "The Chalkeaters feat. Black Gryph0n & PiNKII - FurrÃ½mon- Gotta Smash â€™Em All!.flac" -map_metadata -1 -map 0:0 -metadata artist="The Chalk-Eaters / Black Gryph0n / PiNKII" -metadata album_artist="The Chalk-Eaters" -metadata album="Furrymon - Gotta Smash Them All (Single)" -metadata date="2022-11-25" -metadata genre="Rock | Funk | Rap" -metadata title="Furrymon - Gotta Smash 'em All" -af "apad=pad_dur=3s,volume=-5dB" -c:a libvorbis -q 6 -minrate 128k "Furrymon - Gotta Smash Them All.ogg"
 Move-Item -Path "Furrymon - Gotta Smash Them All.ogg" -Destination "The Chalk-Eaters/(Singles) (Converted)"
 Move-Item -Path "Furrymon - Gotta Smash Them All (thumb).png" -Destination "The Chalk-Eaters/(Singles) (Converted)"
 
