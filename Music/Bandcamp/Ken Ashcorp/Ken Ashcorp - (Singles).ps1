e1c280bc78be8771a63db2c2364f8d1ae4bfbc78
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2023-08-27 02:18:19 -0700

    Update Ken Ashcorp - (Singles).ps1

--- Music/Bandcamp/Ken Ashcorp/Ken Ashcorp - (Singles).ps1
+++ Music/Bandcamp/Ken Ashcorp/Ken Ashcorp - (Singles).ps1
@@ -4,15 +4,16 @@
 New-Item -Path "Ken Ashcorp/(Singles) (Converted)" -ItemType Directory
 
 
-ffmpeg -y -i "Ken Ashcorp - Absolute Territory.flac" -an -vf scale=1024:1024 -sws_flags bicubic "Absolute Territory (thumb).png"
+ffmpeg -y -i "Ken Ashcorp - Absolute Territory.flac" -an -vf scale=1024:1024 -sws_flags bicubic "cover_TMP.png"
 ffmpeg -i "Ken Ashcorp - Absolute Territory.flac" -map_metadata -1 -map 0:0 -metadata artist="Ken Ashcorp" -metadata album_artist="Ken Ashcorp" -metadata album="Absolute Territory (Single)" -metadata date="2013-03-08" -metadata genre="Rock" -metadata title="Absolute Territory" -af "volume=-5dB" -c:a libvorbis -q 6 -minrate 128k "Absolute Territory.ogg"
-Move-Item -Path "Absolute Territory.ogg" -Destination "Ken Ashcorp/(Singles) (Converted)"
-Move-Item -Path "Absolute Territory (thumb).png" -Destination "Ken Ashcorp/(Singles) (Converted)"
+New-Item -Path "Ken Ashcorp/(Singles) (Converted)/Absolute Territory (Single)" -ItemType Directory
+Move-Item -Path "Absolute Territory.ogg" -Destination "Ken Ashcorp/(Singles) (Converted)/Absolute Territory (Single)"
+Move-Item -Path "cover_TMP.png" -Destination "Ken Ashcorp/(Singles) (Converted)/Absolute Territory (Single)/cover.png"
 
-ffmpeg -y -i "Ken Ashcorp - We're Shameless.flac" -an -vf scale=1024:1024 -sws_flags bicubic "We're Shameless (thumb).png"
+ffmpeg -y -i "Ken Ashcorp - We're Shameless.flac" -an -vf scale=1024:1024 -sws_flags bicubic "cover_TMP.png"
 ffmpeg -i "Ken Ashcorp - We're Shameless.flac" -map_metadata -1 -map 0:0 -metadata artist="Ken Ashcorp" -metadata album_artist="Ken Ashcorp" -metadata album="We're Shameless (Single)" -metadata date="2017-07-16" -metadata genre="Rock" -metadata title="We're Shameless" -af "volume=-3dB" -c:a libvorbis -q 6 -minrate 128k "We're Shameless.ogg"
-Move-Item -Path "We're Shameless.ogg" -Destination "Ken Ashcorp/(Singles) (Converted)"
-Move-Item -Path "We're Shameless (thumb).png" -Destination "Ken Ashcorp/(Singles) (Converted)"
-
+New-Item -Path "Ken Ashcorp/(Singles) (Converted)/We're Shameless (Single)" -ItemType Directory
+Move-Item -Path "We're Shameless.ogg" -Destination "Ken Ashcorp/(Singles) (Converted)/We're Shameless (Single)"
+Move-Item -Path "cover_TMP.png" -Destination "Ken Ashcorp/(Singles) (Converted)/We're Shameless (Single)/cover.png"
 
 pause
