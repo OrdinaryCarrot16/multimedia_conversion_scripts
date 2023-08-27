4da8ba22899ccb6345787d5ec7108370e42218e4
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2023-08-27 02:25:23 -0700

    Update Valve - Half-Life 2 - Episode 2 (Sound-Track).ps1

--- Music/Steam/Valve/Valve - Half-Life 2 - Episode 2 (Sound-Track).ps1
+++ Music/Steam/Valve/Valve - Half-Life 2 - Episode 2 (Sound-Track).ps1
@@ -4,8 +4,8 @@
 New-Item -Path "Valve/Half-Life 2 - Episode 2 (Sound-Track) (Converted)" -ItemType Directory
 
 
-ffmpeg -i "cover.png" -an -vf scale=1024:1024 -sws_flags bicubic thumb.png
-Move-Item -Path "thumb.png" -Destination "Valve/Half-Life 2 - Episode 2 (Sound-Track) (Converted)"
+ffmpeg -i "cover.png" -an -vf scale=1024:1024 -sws_flags bicubic cover_TMP.png
+Move-Item -Path "cover_TMP.png" -Destination "Valve/Half-Life 2 - Episode 2 (Sound-Track) (Converted)/cover.png"
 
 
 ffmpeg -i "01 No One Rides For Free.mp3" -map_metadata -1 -map 0:0 -metadata artist="Valve" -metadata album_artist="Valve" -metadata album="Half-Life 2 - Episode 2 (Sound-Track)" -metadata date="2007-10-10" -metadata genre="Sound-Track" -metadata track="1/12" -metadata title="No-One Rides For Free" -af "volume=-3dB" -c:a libvorbis -q 6 -minrate 128k "1 - No-One Rides For Free.ogg"
