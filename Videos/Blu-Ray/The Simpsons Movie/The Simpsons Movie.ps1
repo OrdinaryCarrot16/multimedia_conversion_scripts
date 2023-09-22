478fd314c42a69287b9526cb6518de5df2720c41
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2023-09-21 19:18:55 -0700

    Update The Simpsons Movie.ps1

--- Videos/Blu-Ray/The Simpsons Movie/The Simpsons Movie.ps1
+++ Videos/Blu-Ray/The Simpsons Movie/The Simpsons Movie.ps1
@@ -12,7 +12,7 @@ New-Item -Path "The Simpsons Movie (Converted)" -ItemType Directory
 
 ffmpeg -i "The Simpsons Movie.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 16 "The Simpsons Movie.264"
 ffmpeg -i "The Simpsons Movie.avs" -c:a aac -b:a 192k "The Simpsons Movie.aac"
-mp4box -add "The Simpsons Movie.264" -add "The Simpsons Movie.aac" -new "The Simpsons Movie.mp4"
+mp4box -add "The Simpsons Movie.264" -fps 23.976 -add "The Simpsons Movie.aac" -new "The Simpsons Movie.mp4"
 
 Remove-Item "The Simpsons Movie.264"
 Remove-Item "The Simpsons Movie.aac"
