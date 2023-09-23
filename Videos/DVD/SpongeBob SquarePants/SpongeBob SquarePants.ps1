d4a3567415b1800f097f412babcb3a632dd8eb92
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2023-09-23 00:28:11 -0700

    Update SpongeBob SquarePants.ps1

--- Videos/DVD/SpongeBob SquarePants/SpongeBob SquarePants.ps1
+++ Videos/DVD/SpongeBob SquarePants/SpongeBob SquarePants.ps1
@@ -1,5 +1,5 @@
 # 
-
+<#
 New-Item -Path "MakeMKV/SpongeBob SquarePants" -ItemType Directory
 cd "MakeMKV/SpongeBob SquarePants"
 
@@ -65,7 +65,7 @@ pause
 Write-Host "Insert next disc (Season 1 - Disc 3), then press enter to proceed"
 pause
 
-
+#>
 Write-Host "Push enter to proceed to transcoding, or close app to keep original files"
 pause
 
@@ -82,7 +82,7 @@ Remove-Item "S01E01 - Help Wanted.mkv.ffindex"
 #>
 ffmpeg -i "S01E02 - Reef Blower.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E02 - Reef Blower.264"
 ffmpeg -i "S01E02 - Reef Blower.avs" -c:a aac -b:a 192k "S01E02 - Reef Blower.aac"
-mp4box -add "S01E02 - Reef Blower.264" -fps 29.97 -add "S01E02 - Reef Blower.aac" -new "S01E02 - Reef Blower.mp4"
+mp4box -add "S01E02 - Reef Blower.264" -fps 23.976 -add "S01E02 - Reef Blower.aac" -new "S01E02 - Reef Blower.mp4"
 Move-Item -Path "S01E02 - Reef Blower.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E02 - Reef Blower.mp4"
 Remove-Item "S01E02 - Reef Blower.264"
 Remove-Item "S01E02 - Reef Blower.aac"
@@ -95,3 +95,115 @@ Move-Item -Path "S01E03 - Tea At The Tree-Dome.mp4" -Destination "SpongeBob Squa
 Remove-Item "S01E03 - Tea At The Tree-Dome.264"
 Remove-Item "S01E03 - Tea At The Tree-Dome.aac"
 Remove-Item "S01E03 - Tea At The Tree-Dome.mkv.ffindex"
+
+ffmpeg -i "S01E04 - Bubble-Stand.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E04 - Bubble-Stand.264"
+ffmpeg -i "S01E04 - Bubble-Stand.avs" -c:a aac -b:a 192k "S01E04 - Bubble-Stand.aac"
+mp4box -add "S01E04 - Bubble-Stand.264" -fps 23.976 -add "S01E04 - Bubble-Stand.aac" -new "S01E04 - Bubble-Stand.mp4"
+Move-Item -Path "S01E04 - Bubble-Stand.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E04 - Bubble-Stand.mp4"
+Remove-Item "S01E04 - Bubble-Stand.264"
+Remove-Item "S01E04 - Bubble-Stand.aac"
+Remove-Item "S01E04 - Bubble-Stand.mkv.ffindex"
+
+ffmpeg -i "S01E05 - Ripped Pants.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E05 - Ripped Pants.264"
+ffmpeg -i "S01E05 - Ripped Pants.avs" -c:a aac -b:a 192k "S01E05 - Ripped Pants.aac"
+mp4box -add "S01E05 - Ripped Pants.264" -fps 23.976 -add "S01E05 - Ripped Pants.aac" -new "S01E05 - Ripped Pants.mp4"
+Move-Item -Path "S01E05 - Ripped Pants.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E05 - Ripped Pants.mp4"
+Remove-Item "S01E05 - Ripped Pants.264"
+Remove-Item "S01E05 - Ripped Pants.aac"
+Remove-Item "S01E05 - Ripped Pants.mkv.ffindex"
+
+ffmpeg -i "S01E06 - Jelly-Fishing.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E06 - Jelly-Fishing.264"
+ffmpeg -i "S01E06 - Jelly-Fishing.avs" -c:a aac -b:a 192k "S01E06 - Jelly-Fishing.aac"
+mp4box -add "S01E06 - Jelly-Fishing.264" -fps 23.976 -add "S01E06 - Jelly-Fishing.aac" -new "S01E06 - Jelly-Fishing.mp4"
+Move-Item -Path "S01E06 - Jelly-Fishing.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E06 - Jelly-Fishing.mp4"
+Remove-Item "S01E06 - Jelly-Fishing.264"
+Remove-Item "S01E06 - Jelly-Fishing.aac"
+Remove-Item "S01E06 - Jelly-Fishing.mkv.ffindex"
+
+ffmpeg -i "S01E07 - Plankton.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E07 - Plankton.264"
+ffmpeg -i "S01E07 - Plankton.avs" -c:a aac -b:a 192k "S01E07 - Plankton.aac"
+mp4box -add "S01E07 - Plankton.264" -fps 23.976 -add "S01E07 - Plankton.aac" -new "S01E07 - Plankton.mp4"
+Move-Item -Path "S01E07 - Plankton.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E07 - Plankton.mp4"
+Remove-Item "S01E07 - Plankton.264"
+Remove-Item "S01E07 - Plankton.aac"
+Remove-Item "S01E07 - Plankton.mkv.ffindex"
+
+ffmpeg -i "S01E08 - Naughty Nautical Neighbors.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E08 - Naughty Nautical Neighbors.264"
+ffmpeg -i "S01E08 - Naughty Nautical Neighbors.avs" -c:a aac -b:a 192k "S01E08 - Naughty Nautical Neighbors.aac"
+mp4box -add "S01E08 - Naughty Nautical Neighbors.264" -fps 23.976 -add "S01E08 - Naughty Nautical Neighbors.aac" -new "S01E08 - Naughty Nautical Neighbors.mp4"
+Move-Item -Path "S01E08 - Naughty Nautical Neighbors.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E08 - Naughty Nautical Neighbors.mp4"
+Remove-Item "S01E08 - Naughty Nautical Neighbors.264"
+Remove-Item "S01E08 - Naughty Nautical Neighbors.aac"
+Remove-Item "S01E08 - Naughty Nautical Neighbors.mkv.ffindex"
+
+ffmpeg -i "S01E09 - Boating School.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E09 - Boating School.264"
+ffmpeg -i "S01E09 - Boating School.avs" -c:a aac -b:a 192k "S01E09 - Boating School.aac"
+mp4box -add "S01E09 - Boating School.264" -fps 23.976 -add "S01E09 - Boating School.aac" -new "S01E09 - Boating School.mp4"
+Move-Item -Path "S01E09 - Boating School.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E09 - Boating School.mp4"
+Remove-Item "S01E09 - Boating School.264"
+Remove-Item "S01E09 - Boating School.aac"
+Remove-Item "S01E09 - Boating School.mkv.ffindex"
+
+ffmpeg -i "S01E10 - Pizza Delivery.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E10 - Pizza Delivery.264"
+ffmpeg -i "S01E10 - Pizza Delivery.avs" -c:a aac -b:a 192k "S01E10 - Pizza Delivery.aac"
+mp4box -add "S01E10 - Pizza Delivery.264" -fps 23.976 -add "S01E10 - Pizza Delivery.aac" -new "S01E10 - Pizza Delivery.mp4"
+Move-Item -Path "S01E10 - Pizza Delivery.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E10 - Pizza Delivery.mp4"
+Remove-Item "S01E10 - Pizza Delivery.264"
+Remove-Item "S01E10 - Pizza Delivery.aac"
+Remove-Item "S01E10 - Pizza Delivery.mkv.ffindex"
+
+ffmpeg -i "S01E11 - Home Sweet Pineapple.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E11 - Home Sweet Pineapple.264"
+ffmpeg -i "S01E11 - Home Sweet Pineapple.avs" -c:a aac -b:a 192k "S01E11 - Home Sweet Pineapple.aac"
+mp4box -add "S01E11 - Home Sweet Pineapple.264" -fps 23.976 -add "S01E11 - Home Sweet Pineapple.aac" -new "S01E11 - Home Sweet Pineapple.mp4"
+Move-Item -Path "S01E11 - Home Sweet Pineapple.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E11 - Home Sweet Pineapple.mp4"
+Remove-Item "S01E11 - Home Sweet Pineapple.264"
+Remove-Item "S01E11 - Home Sweet Pineapple.aac"
+Remove-Item "S01E11 - Home Sweet Pineapple.mkv.ffindex"
+
+ffmpeg -i "S01E12 - Mermaid Man And Barnacle Boy.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E12 - Mermaid Man And Barnacle Boy.264"
+ffmpeg -i "S01E12 - Mermaid Man And Barnacle Boy.avs" -c:a aac -b:a 192k "S01E12 - Mermaid Man And Barnacle Boy.aac"
+mp4box -add "S01E12 - Mermaid Man And Barnacle Boy.264" -fps 23.976 -add "S01E11 - Home Sweet Pineapple.aac" -new "S01E12 - Mermaid Man And Barnacle Boy.mp4"
+Move-Item -Path "S01E12 - Mermaid Man And Barnacle Boy.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E12 - Mermaid Man And Barnacle Boy.mp4"
+Remove-Item "S01E12 - Mermaid Man And Barnacle Boy.264"
+Remove-Item "S01E12 - Mermaid Man And Barnacle Boy.aac"
+Remove-Item "S01E12 - Mermaid Man And Barnacle Boy.mkv.ffindex"
+
+ffmpeg -i "S01E13 - Pickles.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E13 - Pickles.264"
+ffmpeg -i "S01E13 - Pickles.avs" -c:a aac -b:a 192k "S01E13 - Pickles.aac"
+mp4box -add "S01E13 - Pickles.264" -fps 23.976 -add "S01E13 - Pickles.aac" -new "S01E13 - Pickles.mp4"
+Move-Item -Path "S01E13 - Pickles.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E13 - Pickles.mp4"
+Remove-Item "S01E13 - Pickles.264"
+Remove-Item "S01E13 - Pickles.aac"
+Remove-Item "S01E13 - Pickles.mkv.ffindex"
+
+ffmpeg -i "S01E14 - Hall Monitor.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E14 - Hall Monitor.264"
+ffmpeg -i "S01E14 - Hall Monitor.avs" -c:a aac -b:a 192k "S01E14 - Hall Monitor.aac"
+mp4box -add "S01E14 - Hall Monitor.264" -fps 23.976 -add "S01E14 - Hall Monitor.aac" -new "S01E14 - Hall Monitor.mp4"
+Move-Item -Path "S01E14 - Hall Monitor.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E14 - Hall Monitor.mp4"
+Remove-Item "S01E14 - Hall Monitor.264"
+Remove-Item "S01E14 - Hall Monitor.aac"
+Remove-Item "S01E14 - Hall Monitor.mkv.ffindex"
+
+ffmpeg -i "S01E15 - Jelly-Fish Jam.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E15 - Jelly-Fish Jam.264"
+ffmpeg -i "S01E15 - Jelly-Fish Jam.avs" -c:a aac -b:a 192k "S01E15 - Jelly-Fish Jam.aac"
+mp4box -add "S01E15 - Jelly-Fish Jam.264" -fps 23.976 -add "S01E15 - Jelly-Fish Jam.aac" -new "S01E15 - Jelly-Fish Jam.mp4"
+Move-Item -Path "S01E15 - Jelly-Fish Jam.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E15 - Jelly-Fish Jam.mp4"
+Remove-Item "S01E15 - Jelly-Fish Jam.264"
+Remove-Item "S01E15 - Jelly-Fish Jam.aac"
+Remove-Item "S01E15 - Jelly-Fish Jam.mkv.ffindex"
+
+ffmpeg -i "S01E16 - Sandys Rocket.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E16 - Sandys Rocket.264"
+ffmpeg -i "S01E16 - Sandys Rocket.avs" -c:a aac -b:a 192k "S01E16 - Sandys Rocket.aac"
+mp4box -add "S01E16 - Sandys Rocket.264" -fps 23.976 -add "S01E16 - Sandys Rocket.aac" -new "S01E16 - Sandys Rocket.mp4"
+Move-Item -Path "S01E16 - Sandys Rocket.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E16 - Sandys Rocket.mp4"
+Remove-Item "S01E16 - Sandys Rocket.264"
+Remove-Item "S01E16 - Sandys Rocket.aac"
+Remove-Item "S01E16 - Sandys Rocket.mkv.ffindex"
+
+ffmpeg -i "S01E17 - Squeaky Boots.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 18 "S01E17 - Squeaky Boots.264"
+ffmpeg -i "S01E17 - Squeaky Boots.avs" -c:a aac -b:a 192k "S01E17 - Squeaky Boots.aac"
+mp4box -add "S01E17 - Squeaky Boots.264" -fps 23.976 -add "S01E17 - Squeaky Boots.aac" -new "S01E17 - Squeaky Boots.mp4"
+Move-Item -Path "S01E17 - Squeaky Boots.mp4" -Destination "SpongeBob SquarePants (Converted)/S01E17 - Squeaky Boots.mp4"
+Remove-Item "S01E17 - Squeaky Boots.264"
+Remove-Item "S01E17 - Squeaky Boots.aac"
+Remove-Item "S01E17 - Squeaky Boots.mkv.ffindex"
