61dbbac33e8980ec0ecc8dd8914697ec15a2ffb3
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2023-09-22 17:17:52 -0700

    Update SpongeBob SquarePants.ps1

--- Videos/DVD/SpongeBob SquarePants/SpongeBob SquarePants.ps1
+++ Videos/DVD/SpongeBob SquarePants/SpongeBob SquarePants.ps1
@@ -19,27 +19,45 @@ Rename-Item -Path "D3_t02.mkv" -NewName "S01E04 - Bubble-Stand.mkv"
 makemkvcon64 --minlength=5 --progress=-same mkv disc:0 03 "./"
 Rename-Item -Path "D4_t03.mkv" -NewName "S01E05 - Ripped Pants.mkv"
 
-makemkvcon64 --minlength=5 --progress=-same mkv disc:0 00 "./"
+makemkvcon64 --minlength=5 --progress=-same mkv disc:0 04 "./"
 Rename-Item -Path "E1_t04.mkv" -NewName "S01E06 - Jelly-Fishing.mkv"
 
-makemkvcon64 --minlength=5 --progress=-same mkv disc:0 00 "./"
-Rename-Item -Path "D1_t00.mkv" -NewName "S01E07 - Reef Blower.mkv"
+makemkvcon64 --minlength=5 --progress=-same mkv disc:0 05 "./"
+Rename-Item -Path "I1_t05.mkv" -NewName "S01E07 - Plankton.mkv"
 
-makemkvcon64 --minlength=5 --progress=-same mkv disc:0 00 "./"
-Rename-Item -Path "D1_t00.mkv" -NewName "S01E08 - Reef Blower.mkv"
+makemkvcon64 --minlength=5 --progress=-same mkv disc:0 06 "./"
+Rename-Item -Path "E3_t06.mkv" -NewName "S01E08 - Naughty Nautical Neighbors.mkv"
 
-makemkvcon64 --minlength=5 --progress=-same mkv disc:0 00 "./"
-Rename-Item -Path "D1_t00.mkv" -NewName "S01E09 - Reef Blower.mkv"
+makemkvcon64 --minlength=5 --progress=-same mkv disc:0 07 "./"
+Rename-Item -Path "E4_t07.mkv" -NewName "S01E09 - Boating School.mkv"
 
-makemkvcon64 --minlength=5 --progress=-same mkv disc:0 00 "./"
-Rename-Item -Path "D1_t00.mkv" -NewName "S01E10 - Reef Blower.mkv"
+makemkvcon64 --minlength=5 --progress=-same mkv disc:0 08 "./"
+Rename-Item -Path "F1_t08.mkv" -NewName "S01E10 - Pizza Delivery.mkv"
+
+makemkvcon64 --minlength=5 --progress=-same mkv disc:0 09 "./"
+Rename-Item -Path "F2_t09.mkv" -NewName "S01E11 - Home Sweet Pineapple.mkv"
+
+makemkvcon64 --minlength=5 --progress=-same mkv disc:0 10 "./"
+Rename-Item -Path "F3_t10.mkv" -NewName "S01E12 - Mermaid Man And Barnacle Boy.mkv"
+
+makemkvcon64 --minlength=5 --progress=-same mkv disc:0 11 "./"
+Rename-Item -Path "F4_t11.mkv" -NewName "S01E13 - Pickles.mkv"
+
+makemkvcon64 --minlength=5 --progress=-same mkv disc:0 12 "./"
+Rename-Item -Path "G1_t12.mkv" -NewName "S01E14 - Hall Monitor.mkv"
+
+makemkvcon64 --minlength=5 --progress=-same mkv disc:0 13 "./"
+Rename-Item -Path "G2_t13.mkv" -NewName "S01E15 - Jelly-Fish Jam.mkv"
+
+makemkvcon64 --minlength=5 --progress=-same mkv disc:0 14 "./"
+Rename-Item -Path "G3_t14.mkv" -NewName "S01E16 - Sandys Rocket.mkv"
+
+makemkvcon64 --minlength=5 --progress=-same mkv disc:0 15 "./"
+Rename-Item -Path "G4_t15.mkv" -NewName "S01E17 - Squeaky Boots.mkv"
 
 
 Write-Host "Insert next disc (Season 1 - Disc 2), then press enter to proceed"
 pause
-
-makemkvcon64 --minlength=0 --progress=-same mkv disc:0 01 "./"
-Rename-Item -Path "C1_t01.mkv" -NewName "E15-E16 - Do The Koopa + Kootie Pie Rocks.mkv"
 
 Write-Host "Insert next disc (Season 1 - Disc 3), then press enter to proceed"
 pause
