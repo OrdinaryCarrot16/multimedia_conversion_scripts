3629d3f1dbb44f627039376967d899d8a6f6d7d6
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2023-09-27 01:03:57 -0700

    yeh

--- Videos/Blu-Ray/Coraline/Coraline.ps1
+++ Videos/Blu-Ray/Coraline/Coraline.ps1
@@ -16,6 +16,7 @@ New-Item -Path "Coraline (Converted)" -ItemType Directory
 ffmpeg -i "Coraline.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 20 "Coraline.264"
 ffmpeg -i "Coraline.avs" -c:a aac -b:a 192k "Coraline.aac"
 mp4box -add "Coraline.264" -fps 23.976 -add "Coraline.aac" -new "Coraline.mp4"
+Move-Item -Path "Coraline.mp4" -Destination "Coraline (Converted)/Coraline.mp4"
 
 Remove-Item "Coraline.264"
 Remove-Item "Coraline.aac"
--- Videos/Blu-Ray/Finding Nemo/Finding Nemo.ps1
+++ Videos/Blu-Ray/Finding Nemo/Finding Nemo.ps1
@@ -1,18 +1,20 @@
+<#
 New-Item -Path "MakeMKV/Finding Nemo" -ItemType Directory
 cd "MakeMKV/Finding Nemo"
 
 # Movie
-makemkvcon64 --minlength=5 --progress=-same mkv disc:0 00(replace with title_id) "./"
-Rename-Item -Path "makemkv_output_name.mkv" -NewName "Finding Nemo.mkv"
-
+makemkvcon64 --minlength=5 --progress=-same mkv disc:0 20 "./"
+Rename-Item -Path "Finding Nemo_t20.mkv" -NewName "Finding Nemo.mkv"
+#>
 Write-Host "Push enter to proceed to transcoding, or close app to keep original files"
 pause
 
-New-Item -Path "dir_name (Converted)" -ItemType Directory
+New-Item -Path "Finding Nemo (Converted)" -ItemType Directory
 
 ffmpeg -i "Finding Nemo.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 20 "Finding Nemo_TMP.264"
 ffmpeg -i "Finding Nemo.avs" -c:a aac -b:a 192k "Finding Nemo_TMP.aac"
 mp4box -add "Finding Nemo_TMP.264" -fps 23.976 -add "Finding Nemo_TMP.aac" -new "Finding Nemo.mp4"
+Move-Item -Path "Finding Nemo.mp4" -Destination "Finding Nemo (Converted)/Finding Nemo.mp4"
 
 Remove-Item "Finding Nemo_TMP.264"
 Remove-Item "Finding Nemo_TMP.aac"
