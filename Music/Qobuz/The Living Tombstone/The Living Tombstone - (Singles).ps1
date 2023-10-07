06188fd1492cb976576343e29cddb08d423bfcb2
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2023-10-06 23:58:25 -0700

    Update The Living Tombstone - (Singles).ps1

--- Music/Qobuz/The Living Tombstone/The Living Tombstone - (Singles).ps1
+++ Music/Qobuz/The Living Tombstone/The Living Tombstone - (Singles).ps1
@@ -30,7 +30,7 @@ Move-Item -Path "Die In A Fire.ogg" -Destination "The Living Tombstone/(Singles)
 Move-Item -Path "cover_TMP.png" -Destination "The Living Tombstone/(Singles) (Converted)/Die In A Fire (Single)/cover.png"
 
 ffmpeg -y -i "echo-the-living-tombstone.jpg" -an -vf scale=1024:1024 -sws_flags bicubic "cover_TMP.png"
-ffmpeg -ss 00:00:00.200 -to 00:03:17.500 -i "01-01 Echo (feat. Crusher-P).flac" -map_metadata -1 -map 0:0 -metadata artist="The Living Tombstone / Crusher-P" -metadata album_artist="The Living Tombstone" -metadata album="Echo (Single)" -metadata date="2017-05-31" -metadata genre="Electronic" -metadata title="Echo (Remix)" -af "apad=pad_dur=3s,volume=-5dB" -c:a libvorbis -q 6 -minrate 128k "Echo (Remix).ogg"
+ffmpeg -ss 00:00:00.200 -to 00:03:17.500 -i "01-01 Echo (feat. Crusher-P).flac" -map_metadata -1 -map 0:0 -metadata artist="The Living Tombstone / Crusher-P" -metadata album_artist="The Living Tombstone" -metadata album="Echo (Single) (Remix)" -metadata date="2017-05-31" -metadata genre="Electronic" -metadata title="Echo (Remix)" -af "apad=pad_dur=3s,volume=-5dB" -c:a libvorbis -q 6 -minrate 128k "Echo (Remix).ogg"
 New-Item -Path "The Living Tombstone/(Singles) (Converted)/Echo (Single) (Remix)" -ItemType Directory
 Move-Item -Path "Echo (Remix).ogg" -Destination "The Living Tombstone/(Singles) (Converted)/Echo (Single) (Remix)"
 Move-Item -Path "cover_TMP.png" -Destination "The Living Tombstone/(Singles) (Converted)/Echo (Single) (Remix)/cover.png"
@@ -42,7 +42,7 @@ Move-Item -Path "It's Been So Long.ogg" -Destination "The Living Tombstone/(Sing
 Move-Item -Path "cover_TMP.png" -Destination "The Living Tombstone/(Singles) (Converted)/It's Been So Long (Single)/cover.png"
 
 ffmpeg -y -i "long-time-friends-the-living-tombstone.jpg" -an -vf scale=1024:1024 -sws_flags bicubic "cover_TMP.png"
-ffmpeg -ss 00:00:00.100 -to 00:04:15.500 -i "01-01 Long Time Friends.flac" -map_metadata -1 -map 0:0 -metadata artist="The Living Tombstone" -metadata album_artist="The Living Tombstone" -metadata album="Long Time Friends (Spooky Mix) (Remix) (Single)" -metadata date="2020-10-30" -metadata genre="Electronic" -metadata title="Long Time Friends (Spooky Mix) (Remix)" -af "apad=pad_dur=3s,volume=-3dB" -c:a libvorbis -q 6 -minrate 128k "Long Time Friends (Spooky Mix) (Single) (Remix).ogg"
+ffmpeg -ss 00:00:00.100 -to 00:04:15.500 -i "01-01 Long Time Friends.flac" -map_metadata -1 -map 0:0 -metadata artist="The Living Tombstone" -metadata album_artist="The Living Tombstone" -metadata album="Long Time Friends (Spooky Mix) (Single) (Remix)" -metadata date="2020-10-30" -metadata genre="Electronic" -metadata title="Long Time Friends (Spooky Mix) (Remix)" -af "apad=pad_dur=3s,volume=-3dB" -c:a libvorbis -q 6 -minrate 128k "Long Time Friends (Spooky Mix) (Single) (Remix).ogg"
 New-Item -Path "The Living Tombstone/(Singles) (Converted)/Long Time Friends (Spooky Mix) (Single) (Remix)" -ItemType Directory
 Move-Item -Path "Long Time Friends (Spooky Mix) (Single) (Remix).ogg" -Destination "The Living Tombstone/(Singles) (Converted)/Long Time Friends (Spooky Mix) (Single) (Remix)"
 Move-Item -Path "cover_TMP.png" -Destination "The Living Tombstone/(Singles) (Converted)/Long Time Friends (Spooky Mix) (Single) (Remix)/cover.png"
