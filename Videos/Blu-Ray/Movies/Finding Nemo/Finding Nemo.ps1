24d82133eafa7e8fdec1ba1d6c3b1d965c591793
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2024-03-14 12:18:42 -0700

    Update Finding Nemo.ps1

--- Videos/Blu-Ray/Movies/Finding Nemo/Finding Nemo.ps1
+++ Videos/Blu-Ray/Movies/Finding Nemo/Finding Nemo.ps1
@@ -2,13 +2,15 @@
 New-Item -Path "MakeMKV/Finding Nemo" -ItemType Directory
 cd "MakeMKV/Finding Nemo"
 
+$rip_encode=Read-Host -Prompt "Would you like to 'rip' (1) the Finding Nemo movie, or 're-encode' (2) an existing rip (made using this script)"
+
+if ($rip_encode -eq "1") {
 # Movie
 makemkvcon64 --minlength=5 --progress=-same mkv disc:0 20 "./"
 Rename-Item -Path "Finding Nemo_t20.mkv" -NewName "Finding Nemo.mkv"
+}
 
-Write-Host "Push enter to proceed to transcoding, or close app to keep original files"
-pause
-
+if ($rip_encode -eq "2") {
 New-Item -Path "Finding Nemo (Converted)" -ItemType Directory
 
 ffmpeg -i "Finding Nemo.avs" -c:v libx264 -tune film -profile:v main -preset slow -crf 20 "Finding Nemo_TMP.264"
@@ -19,5 +21,6 @@ Move-Item -Path "Finding Nemo.mp4" -Destination "Finding Nemo (Converted)/Findin
 Remove-Item "Finding Nemo_TMP.264"
 Remove-Item "Finding Nemo_TMP.aac"
 Remove-Item "Finding Nemo.mkv.ffindex"
+}
 
 pause
