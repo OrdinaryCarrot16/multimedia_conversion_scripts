f6d41b1561f016ca3887d083bd68174a831b4c56
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2024-03-14 12:23:41 -0700

    Update The SpongeBob SquarePants Movie.ps1

--- Videos/Blu-Ray/Movies/The SpongeBob SquarePants Movie/The SpongeBob SquarePants Movie.ps1
+++ Videos/Blu-Ray/Movies/The SpongeBob SquarePants Movie/The SpongeBob SquarePants Movie.ps1
@@ -1,12 +1,19 @@
 New-Item -Path "MakeMKV/The SpongeBob SquarePants Movie" -ItemType Directory
 cd "MakeMKV/The SpongeBob SquarePants Movie"
 
+$rip_encode=Read-Host -Prompt "Would you like to 'rip' (1) the SpongeBob SquarePants movie, or 're-encode' (2) an existing rip (made using this script)"
+
+if ($rip_encode -eq "1") {
+
+Write-Host "Insert disc, then press enter to proceed"
+pause
+
 # Movie
 makemkvcon64 --minlength=5 --progress=-same mkv disc:0 04 "./"
 Rename-Item -Path "The SpongeBob SquarePants Movie_t04.mkv" -NewName "The SpongeBob SquarePants Movie.mkv"
+}
 
-Write-Host "Push enter to proceed to transcoding, or close app to keep original files"
-pause
+if ($rip_encode -eq "2") {
 
 New-Item -Path "The SpongeBob SquarePants Movie (Converted)" -ItemType Directory
 
@@ -18,5 +25,6 @@ Move-Item -Path "The SpongeBob SquarePants Movie.mp4" -Destination "The SpongeBo
 Remove-Item "The SpongeBob SquarePants Movie_TMP.264"
 Remove-Item "The SpongeBob SquarePants Movie_TMP.aac"
 Remove-Item "The SpongeBob SquarePants Movie.mkv.ffindex"
+}
 
 pause
